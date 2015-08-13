652740dfac75461a939e52d2783fd075ef9dcc10
Eric O <elo2112@columbia.edu>
2015-08-13 17:41:28 -0400

    Work on csv import/export test fixtures

--- lib/hyacinth/utils/csv_import_export_utils.rb
+++ lib/hyacinth/utils/csv_import_export_utils.rb
@@ -1,11 +1,11 @@
-class Hyacinth::Utils::CsvImportExport
+class Hyacinth::Utils::CsvImportExportUtils
 
-  def self.csv_to_json(csv)
-    
+  def self.csv_to_json(csv_data_string)
+    return ''
   end
   
   def self.json_to_csv(json)
-    
+    return ''
   end
 
 end
--- spec/unit/lib_hyacinth_utils/csv_import_export_spec.rb
+++ spec/unit/lib_hyacinth_utils/csv_import_export_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-context 'Hyacinth::Utils::CsvImportExport' do
+context 'Hyacinth::Utils::CsvImportExportUtils' do
 
   before(:all) do
     
@@ -13,7 +13,10 @@ context 'Hyacinth::Utils::CsvImportExport' do
   context ".csv_to_json" do
     it "converts properly" do
       
-      expect(1).to eq(1)
+      expected_json_output = fixture('lib/hyacinth/utils/csv_import_export/sample_record.json').read
+      json_output = Hyacinth::Utils::CsvImportExportUtils.csv_to_json('')
+      
+      expect(json_output).to eq(expected_json_output)
     end
   end
   
