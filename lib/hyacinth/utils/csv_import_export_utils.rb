c767a25d9547d7ba16f8ef451c5dc0e0d3af9713
Fred Duby <fcd1@columbia.edu>
2015-08-14 15:10:41 -0400

    Incomplete implementation of CSV to digital object conversion

--- lib/hyacinth/utils/csv_import_export_utils.rb
+++ lib/hyacinth/utils/csv_import_export_utils.rb
@@ -1,11 +1,51 @@
 class Hyacinth::Utils::CsvImportExportUtils
 
-  def self.csv_to_json(csv_data_string)
-    return ''
+  def self.csv_to_digital_object_data(csv_data_string)
+
+    line_counter = -1
+    column_indices_to_headers = nil
+    digital_object_data_results = []
+
+    CSV.parse(csv_data_string) do |row|
+
+      line_counter += 1
+
+      # first line is human readable, so we ignore it
+      if line_counter == 0
+        next
+      end
+
+      # second line is the real header line, so store it as such
+      if line_counter == 1 
+        column_indices_to_headers = row
+        next
+      end
+
+      # process the rest of the lines ...
+      digital_object_data = {}
+      row.each_with_index do |cell_value,index|
+
+        next if cell_value.blank?
+
+        if column_indices_to_headers[index].start_with?('_')
+          digital_object_data[ column_indices_to_headers[index] ] ||= []
+          digital_object_data[ column_indices_to_headers[index] ] << cell_value
+        end
+
+      end
+
+      digital_object_data_results << digital_object_data unless digital_object_data.blank?
+
+    end
+    
+    return digital_object_data_results
+
   end
-  
-  def self.json_to_csv(json)
+
+  def self.digital_object_data_to_csv(digital_object_data)
+
     return ''
+
   end
 
 end
--- spec/unit/lib_hyacinth_utils/csv_import_export_spec.rb
+++ spec/unit/lib_hyacinth_utils/csv_import_export_spec.rb
@@ -13,18 +13,22 @@ context 'Hyacinth::Utils::CsvImportExportUtils' do
   context ".csv_to_json" do
     it "converts properly" do
       
-      expected_json_output = fixture('lib/hyacinth/utils/csv_import_export/sample_record.json').read
-      json_output = Hyacinth::Utils::CsvImportExportUtils.csv_to_json('')
-      
-      expect(json_output).to eq(expected_json_output)
+      expected_digital_object_data = JSON.parse( fixture('lib/hyacinth/utils/csv_import_export/sample_record.json').read )
+      csv_input = fixture('lib/hyacinth/utils/csv_import_export/sample_record.csv').read
+      digital_object_data = Hyacinth::Utils::CsvImportExportUtils.csv_to_digital_object_data(csv_input)
+      expect(digital_object_data).to eq(expected_digital_object_data)
+
     end
   end
   
   context ".json_to_csv" do
+
     it "converts properly" do
       
       expect(1).to eq(1)
+
     end
+
   end
 
 end
