fb24d21dcdfc11c14227c4d0ab33e02b2defe241
Eric O <elo2112@columbia.edu>
2015-08-26 13:41:30 -0400

    Continue with csv-to-digital_object_data implementation

--- lib/hyacinth/utils/csv_import_export_utils.rb
+++ lib/hyacinth/utils/csv_import_export_utils.rb
@@ -1,13 +1,15 @@
 class Hyacinth::Utils::CsvImportExportUtils
+  
+  ##############################
+  # CSV to Digital Object Data #
+  ##############################
 
   def self.csv_to_digital_object_data(csv_data_string)
-
     line_counter = -1
     column_indices_to_headers = nil
     digital_object_data_results = []
 
     CSV.parse(csv_data_string) do |row|
-
       line_counter += 1
 
       # first line is human readable, so we ignore it
@@ -22,25 +24,45 @@ class Hyacinth::Utils::CsvImportExportUtils
       end
 
       # process the rest of the lines ...
-      digital_object_data = {}
-      row.each_with_index do |cell_value,index|
-
-        next if cell_value.blank?
-
-        if column_indices_to_headers[index].start_with?('_')
-          digital_object_data[ column_indices_to_headers[index] ] ||= []
-          digital_object_data[ column_indices_to_headers[index] ] << cell_value
-        end
-
-      end
-
+      digital_object_data = self.process_csv_row(column_indices_to_headers, row)
       digital_object_data_results << digital_object_data unless digital_object_data.blank?
-
     end
     
     return digital_object_data_results
-
   end
+  
+  # Process a single CSV data row and return digital_object_data
+  def self.process_csv_row(column_indices_to_headers, row_data)
+    digital_object_data = {}
+    current_builder_path = [] # e.g. ['name', 'name_role']
+    
+    row_data.each_with_index do |cell_value,index|
+      next if cell_value.blank?
+      
+      # Handle internal field, which is named with a leading underscore
+      if column_indices_to_headers[index].start_with?('_')
+        
+        self.process_internal_field_value(digital_object_data, cell_value, column_indices_to_headers[index])
+      else
+        # Handle dynamic field
+      end
+
+    end
+    
+  end
+  
+  def self.process_internal_field_value(digital_object_data, value, internal_field_header_name)
+    digital_object_data[ internal_field_header_name ] ||= []
+    digital_object_data[ internal_field_header_name ] << value
+  end
+
+
+
+
+
+  ##############################
+  # Digital Object Data to CSV #
+  ##############################
 
   def self.digital_object_data_to_csv(digital_object_data)
 
--- spec/unit/lib_hyacinth_utils/csv_import_export_spec.rb
+++ spec/unit/lib_hyacinth_utils/csv_import_export_spec.rb
@@ -2,33 +2,30 @@ require 'rails_helper'
 
 context 'Hyacinth::Utils::CsvImportExportUtils' do
 
-  before(:all) do
+  before(:context) do
+    @expected_digital_object_data = JSON.parse( fixture('lib/hyacinth/utils/csv_import_export/sample_record.json').read )
+  end
+
+  before(:example) do
     
   end
 
-  before(:each) do
-    
-  end
-
-  context ".csv_to_json" do
+  describe ".csv_to_digital_object_data" do
     it "converts properly" do
-      
-      expected_digital_object_data = JSON.parse( fixture('lib/hyacinth/utils/csv_import_export/sample_record.json').read )
-      csv_input = fixture('lib/hyacinth/utils/csv_import_export/sample_record.csv').read
-      digital_object_data = Hyacinth::Utils::CsvImportExportUtils.csv_to_digital_object_data(csv_input)
-      expect(digital_object_data).to eq(expected_digital_object_data)
-
+      #@digital_object_data = Hyacinth::Utils::CsvImportExportUtils.csv_to_digital_object_data(fixture('lib/hyacinth/utils/csv_import_export/sample_record.csv').read)
+      skip 'Implementation pending'
     end
   end
-  
-  context ".json_to_csv" do
 
-    it "converts properly" do
+  describe ".process_internal_field_value" do
+    it "works" do
+      digital_object_data = {}
+      value = 'abc:123'
+      internal_field_header_name = '_pid'
+      Hyacinth::Utils::CsvImportExportUtils.process_internal_field_value(digital_object_data, value, internal_field_header_name)
       
-      expect(1).to eq(1)
-
+      expect(digital_object_data).to eq({'_pid' => ['abc:123']})
     end
-
   end
 
 end
