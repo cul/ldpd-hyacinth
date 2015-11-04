ff6fb52c2a21e4c316849264297284dfbc7a4d3a
Eric O <eric@ejdigitalmedia.com>
2015-11-04 13:00:13 -0500

    Controlled Term selection/display improvements

--- app/assets/javascripts/general/terms.js
+++ app/assets/javascripts/general/terms.js
@@ -12,15 +12,15 @@ Hyacinth.Term.hideAndShowFields = function(){
   var $addOrEditTermForm = $('#add-or-edit-term-form');
   
   if (type == 'external') {
-    $addOrEditTermForm.find('.uri-field').show();
+    $addOrEditTermForm.find('.term-uri-field').show();
     $addOrEditTermForm.find('.term-additional-field').show();
   } else if (type == 'local') {
-    $addOrEditTermForm.find('.uri-field').hide();
-    $addOrEditTermForm.find('.uri-field').find('input').val(''); // clear value of uri input
+    $addOrEditTermForm.find('.term-uri-field').hide();
+    $addOrEditTermForm.find('.term-uri-field').find('input').val(''); // clear value of uri input
     $addOrEditTermForm.find('.term-additional-field').show();
   } else if (type == 'temporary') {
-    $addOrEditTermForm.find('.uri-field').hide().val('');
-    $addOrEditTermForm.find('.uri-field').find('input').val(''); // clear value of uri input
+    $addOrEditTermForm.find('.term-uri-field').hide().val('');
+    $addOrEditTermForm.find('.term-uri-field').find('input').val(''); // clear value of uri input
     $addOrEditTermForm.find('.term-additional-field').hide();
     $addOrEditTermForm.find('.term-additional-field').find('input').val(''); // clear values of child inputs
   }
