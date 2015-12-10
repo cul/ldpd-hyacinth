2deacf5bf828eb2e01e6e457eda7d06208b78f59
Benjamin Armintor <armintor@gmail.com>
2015-12-10 11:50:45 -0500

    Hyacinth::Utils::HashUtils style conformance

--- lib/hyacinth/utils/hash_utils.rb
+++ lib/hyacinth/utils/hash_utils.rb
@@ -1,32 +1,30 @@
 class Hyacinth::Utils::HashUtils
-
   def self.find_nested_hash_values(obj, key)
     values = []
-    
+
     if obj.respond_to?(:key?) && obj.key?(key)
       values << obj[key]
     elsif obj.respond_to?(:each)
       obj.each do |element|
-        values += find_nested_hash_values(element,key)
+        values += find_nested_hash_values(element, key)
       end
     end
-    
-    return values
+
+    values
   end
-  
+
   # Finds any nested hash that contains the specified key
   def self.find_nested_hashes_that_contain_key(obj, key)
     values = []
-    
+
     if obj.respond_to?(:key?) && obj.key?(key)
       values << obj
     elsif obj.respond_to?(:each)
       obj.each do |element|
-        values += self.find_nested_hashes_that_contain_key(element,key)
+        values += find_nested_hashes_that_contain_key(element, key)
       end
     end
-    
-    return values
-  end
 
+    values
+  end
 end
