1f121cba7594bd7973ae29e0d99dea1b3c30688a
Benjamin Armintor <armintor@gmail.com>
2015-12-10 14:11:57 -0500

    repository_cache initializer style conformance

--- config/initializers/repository_cache.rb
+++ config/initializers/repository_cache.rb
@@ -1,1 +1,1 @@
-REPOSITORY_CACHE_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/repository_cache.yml")[Rails.env]
+REPOSITORY_CACHE_CONFIG = YAML.load_file("#{Rails.root}/config/repository_cache.yml")[Rails.env]
