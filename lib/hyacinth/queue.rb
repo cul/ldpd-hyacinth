193c58bca494af45cc34b678029a3176f4e2a8f0
Benjamin Armintor <armintor@gmail.com>
2015-12-10 13:50:35 -0500

    Hyacinth::Queue style conformance

--- lib/hyacinth/queue.rb
+++ lib/hyacinth/queue.rb
@@ -1,24 +1,22 @@
 module Hyacinth::Queue
-
   DIGITAL_OBJECT_IMPORT = :digital_object_import
   DIGITAL_OBJECT_CSV_EXPORT = :digital_object_csv_export
 
   QUEUES_IN_DESCENDING_PRIORITY_ORDER = [DIGITAL_OBJECT_CSV_EXPORT, DIGITAL_OBJECT_IMPORT]
-  
+
   def self.process_digital_object_import(digital_object_import_id)
     if HYACINTH['queue_long_jobs']
-			Resque.enqueue(ProcessDigitalObjectImportJob, digital_object_import_id)
-		else
-			ProcessDigitalObjectImportJob.perform(digital_object_import_id)
-		end
-  end
-  
-  def self.export_search_results_to_csv(csv_export_id)
-    if HYACINTH['queue_long_jobs']
-			Resque.enqueue(ExportSearchResultsToCsvJob, csv_export_id)
-		else
-			ExportSearchResultsToCsvJob.perform(csv_export_id)
-		end
+      Resque.enqueue(ProcessDigitalObjectImportJob, digital_object_import_id)
+    else
+      ProcessDigitalObjectImportJob.perform(digital_object_import_id)
+    end
   end
 
+  def self.export_search_results_to_csv(csv_export_id)
+    if HYACINTH['queue_long_jobs']
+      Resque.enqueue(ExportSearchResultsToCsvJob, csv_export_id)
+    else
+      ExportSearchResultsToCsvJob.perform(csv_export_id)
+    end
+  end
 end
