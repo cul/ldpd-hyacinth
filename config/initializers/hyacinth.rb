5b4abd36c436cc74bd651d7d895465bbf645849a
Benjamin Armintor <armintor@gmail.com>
2015-12-10 14:08:41 -0500

    hyacinth initializer style conformance

--- config/initializers/hyacinth.rb
+++ config/initializers/hyacinth.rb
@@ -1,14 +1,16 @@
-HYACINTH = YAML.load_file("#{Rails.root.to_s}/config/hyacinth.yml")[Rails.env]
+HYACINTH = YAML.load_file("#{Rails.root}/config/hyacinth.yml")[Rails.env]
 
-puts '---------------------------'
-puts 'Initializing Hyacinth in environment: ' + Rails.env
-puts '---------------------------'
-puts 'Rails ENV: ' + Rails.env
-puts 'Fedora URL: ' + ActiveFedora.config.credentials[:url]
-puts 'Hydra Solr URL: ' + ActiveFedora.solr_config[:url]
-puts 'Hyacinth Solr URL: ' + HYACINTH['solr_url']
-puts '---------------------------'
-puts ''
+Hyacinth::Utils::Logger.logger.tap do |logger|
+  logger.info '---------------------------'
+  logger.info 'Initializing Hyacinth in environment: ' + Rails.env
+  logger.info '---------------------------'
+  logger.info 'Rails ENV: ' + Rails.env
+  logger.info 'Fedora URL: ' + ActiveFedora.config.credentials[:url]
+  logger.info 'Hydra Solr URL: ' + ActiveFedora.solr_config[:url]
+  logger.info 'Hyacinth Solr URL: ' + HYACINTH['solr_url']
+  logger.info '---------------------------'
+  logger.info ''
+end
 
 raise 'Error: Please set a value for publish_target_api_key_encryption_key in your hyacinth.yml file' if HYACINTH['publish_target_api_key_encryption_key'].blank?
 
@@ -19,24 +21,3 @@ Rails.backtrace_cleaner.remove_silencers! if Rails.env.development?
 FileUtils.mkdir_p(HYACINTH['upload_directory']) if HYACINTH['upload_directory']
 FileUtils.mkdir_p(HYACINTH['default_asset_home']) if HYACINTH['default_asset_home']
 FileUtils.mkdir_p(HYACINTH['csv_export_directory']) if HYACINTH['csv_export_directory']
-
-#
-## Raise error if default pid generator is not specified
-#raise 'Missing default pid generator.  Please set one in your hyacinth.yml file.' if HYACINTH['default_pid_generator_namespace'].nil?
-#
-## Raise error if we cannot connect to Fedora
-#content_aggregator_cmodel = nil
-#begin
-#  content_aggregator_cmodel = ActiveFedora::Base.find('ldpd:ContentAggregator') # Attempt to retrieve
-#rescue Errno::ECONNREFUSED => e
-#  raise 'Error: Could not connect to fedora. (' + e.message + ')'
-#end
-#
-## Raise error if required ContentModels haven't been loaded yet
-#if content_aggregator_cmodel.nil?
-#  begin
-#    content_aggregator_cmodel = ActiveFedora::Base.find('ldpd:ContentAggregator') # Attempt to retrieve
-#  rescue Errno::ECONNREFUSED => e
-#    raise 'Error: Could not find ContentAggregator ContentModel.  Have you loaded all required ContentModel? (' + e.message + ')'
-#  end
-#end
