d13aef906868d52ff4d816dc389b9a5c838815b0
Benjamin Armintor <armintor@gmail.com>
2015-12-10 14:10:31 -0500

    term_additional_fields intiializer style conformance

--- config/initializers/term_additional_fields.rb
+++ config/initializers/term_additional_fields.rb
@@ -1,1 +1,1 @@
-TERM_ADDITIONAL_FIELDS = YAML.load_file("#{Rails.root.to_s}/config/term_additional_fields.yml")[Rails.env]
+TERM_ADDITIONAL_FIELDS = YAML.load_file("#{Rails.root}/config/term_additional_fields.yml")[Rails.env]
