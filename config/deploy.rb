5c89914e026ea60a24b60fe54a65a76634c61a37
Eric O <elo2112@columbia.edu>
2016-01-08 17:17:01 -0500

    Version 0.4.03; Symlink tmp/pids directory for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :linked_files, fetch(:linked_files, []).push(
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, fetch(:linked_dirs, []).push('log')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
