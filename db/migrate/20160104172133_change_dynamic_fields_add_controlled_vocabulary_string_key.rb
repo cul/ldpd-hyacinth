2e2946649550332d16f75f4dfaac4d21480581d2
Eric O <elo2112@columbia.edu>
2016-01-13 15:45:47 -0500

    Fix for addition of index on dynamic_fields table column controlled_vocabulary_string_key; Schema update

--- db/migrate/20160104172133_change_dynamic_fields_add_controlled_vocabulary_string_key.rb
+++ db/migrate/20160104172133_change_dynamic_fields_add_controlled_vocabulary_string_key.rb
@@ -2,7 +2,9 @@ class ChangeDynamicFieldsAddControlledVocabularyStringKey < ActiveRecord::Migrat
   def change
     change_table :dynamic_fields do |t|
       t.remove_references :controlled_vocabulary
-      t.string :controlled_vocabulary_string_key, null: true, index: true
+      t.string :controlled_vocabulary_string_key, null: true
     end
+    
+    add_index :dynamic_fields, :controlled_vocabulary_string_key
   end
 end
