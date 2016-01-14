fb8e32dd99a597d1b369b7fdc1a4109ff2218ec0
Eric O <eric@ejdigitalmedia.com>
2016-01-13 21:27:30 -0500

    Deleting a CsvExport should also delete its associated csv file

--- app/models/csv_export.rb
+++ app/models/csv_export.rb
@@ -1,8 +1,17 @@
 class CsvExport < ActiveRecord::Base
   
+  after_destroy :delete_associated_file_if_exists
+  
   belongs_to :user
   
   enum status: {pending: 0, success: 1, failure: 2}
   serialize :export_errors, Array
+  
+  def delete_associated_file_if_exists
+    if self.path_to_csv_file.present? && File.exists?(self.path_to_csv_file)
+        FileUtils.rm(self.path_to_csv_file)
+    end
+    
+  end
   
 end
