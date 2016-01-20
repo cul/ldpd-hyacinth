e18041b1eb721d4cf6f42ab461a972ae60143acc
Eric O <eric@ejdigitalmedia.com>
2016-01-19 23:49:49 -0500

    Paginated Digital Object Import view with clickable filters for Digital Objct Import status; Show CSV Row Number on Digital Object Import show page

--- app/controllers/digital_object_imports_controller.rb
+++ app/controllers/digital_object_imports_controller.rb
@@ -1,49 +1,22 @@
 class DigitalObjectImportsController < ApplicationController
 
-  # fcd1, 10/19/15: TODO: implement access control:
-  # only admin type user can see all DigitalObjectImports
   def index
-    
-    # params[:import_job_id] will be set if coming in via an import_job, i.e.
-    # path helper: import_job_digital_object_imports_path
-    if params.has_key? :import_job_id
+    @import_job = ImportJob.find params[:import_job_id] if params.has_key? :import_job_id
 
-      # only want to show associated DigitalObjectImports
-      @import_job = ImportJob.find params[:import_job_id]
-      @digital_object_imports = @import_job.digital_object_imports.all
-      @sub_header = 'for ' + @import_job.name
+    @status_filter = params[:status].present? && DigitalObjectImport.statuses.include?(params[:status].to_sym) ? params[:status].to_sym : nil
+    page = params[:page]
+    per_page = 50
 
+    if current_user.is_admin?
+      @digital_object_imports = @status_filter.present? ? DigitalObjectImport.where(status: DigitalObjectImport.statuses[@status_filter]).page(page).per(per_page) : DigitalObjectImport.all.page(page).per(per_page)
     else
-
-      # coming in directly, i.e.
-      # path helper: digital_object_imports_path
-      # show all DigitalObjectImports
-
-      # TODO: access control, only admins should see 
-      # all DigitalObjectImports
-      @digital_object_imports = DigitalObjectImport.all
-      @sub_header = 'for all import jobs'
-
+      @digital_object_imports = @status_filter.present? ? DigitalObjectImport.where(user: current_user, status: DigitalObjectImport.statuses[@status_filter]).page(page).per(per_page) : DigitalObjectImport.where(user: current_user).page(page).per(per_page)
     end
-
   end
 
   def show
-
-    # two possible helper paths to get here:
-    # first path:
-    # import_job_digital_object_import_path
-    # which means we came from a listing of DigitalObjectImports for a particular ImportJob
-    # In this case, params[:import_job_id] is set
-    # second path:
-    # digital_object_import
-    # which means we came from the listing of all DigitalObjectImports
-    # 
-    # Associated view will be slightly different depending on which path was used
-    
     @import_job = ImportJob.find params[:import_job_id] if params.has_key? :import_job_id
     @digital_object_import = DigitalObjectImport.find(params[:id])
-
   end
 
 end
