62731118bd2f01690ad43a7331ee15591ceec1da
Eric O <elo2112@columbia.edu>
2016-01-21 14:20:38 -0500

    Increase jetty startup wait time

--- app/jobs/reindex_digital_object_job.rb
+++ app/jobs/reindex_digital_object_job.rb
@@ -2,6 +2,7 @@ class ReindexDigitalObjectJob
   @queue = Hyacinth::Queue::DIGITAL_OBJECT_REINDEX
 
   def self.perform(digital_object_pid)
-    DigitalObject::Base.find(digital_object_pid).update_index(false)
+    DigitalObject::Base.find(digital_object_pid).update_index(false)  # Pass false param to update_index() method so that we don't do
+                                                                      # a Solr commit for each update (because that would be inefficient).
   end
 end
