b13bdbdb5187c3a081117c0afbeb355d64c791d5
Eric O <elo2112@columbia.edu>
2016-01-21 14:24:18 -0500

    Reformat comment to make RuboCop happy

--- app/jobs/reindex_digital_object_job.rb
+++ app/jobs/reindex_digital_object_job.rb
@@ -2,7 +2,7 @@ class ReindexDigitalObjectJob
   @queue = Hyacinth::Queue::DIGITAL_OBJECT_REINDEX
 
   def self.perform(digital_object_pid)
-    DigitalObject::Base.find(digital_object_pid).update_index(false)  # Pass false param to update_index() method so that we don't do
-                                                                      # a Solr commit for each update (because that would be inefficient).
+    # Pass false param to update_index() method so that we don't do a Solr commit for each update (because that would be inefficient).
+    DigitalObject::Base.find(digital_object_pid).update_index(false)
   end
 end
