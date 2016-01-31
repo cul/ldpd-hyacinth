da01e1baaecbb697bb9970cf1452f8f22ebc4c22
Eric O <eric@ejdigitalmedia.com>
2016-01-30 22:56:21 -0500

    Version 2.0.15; Display more human-friendly file size units

--- app/assets/javascripts/digital_objects_app/models/digital_object/asset.js
+++ app/assets/javascripts/digital_objects_app/models/digital_object/asset.js
@@ -18,6 +18,26 @@ Hyacinth.DigitalObjectsApp.DigitalObject.Asset.prototype.getFileSizeInBytes = fu
   return this.assetData['file_size_in_bytes'];
 };
 
+Hyacinth.DigitalObjectsApp.DigitalObject.Asset.prototype.getFileSizeString = function() {
+  var sizeInBytes = parseInt(this.assetData['file_size_in_bytes']);
+  var value = sizeInBytes;
+  var unit = 'B';
+	if(sizeInBytes < 1000) {
+  	return value + ' ' + unit;
+  } else if(sizeInBytes < 1000000) {
+  	value = sizeInBytes/1000;
+    unit = 'kB';
+  } else if(sizeInBytes < 1000000000) {
+  	value = sizeInBytes/1000000;
+    unit = 'MB';
+  } else {
+	  value = sizeInBytes/1000000000;
+    unit = ' GB';
+  }
+  
+  return parseFloat(parseFloat(value).toFixed(2)) + ' ' + unit; // the outermost parseFloat call removes trailing ".00" if present, but leaves values like ".01"
+};
+
 Hyacinth.DigitalObjectsApp.DigitalObject.Asset.prototype.getOriginalFilename = function(){
   return this.assetData['original_filename'];
 };
