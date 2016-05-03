170a4f17a909ed1900373e84be78b2506a4408d4
Fred Duby <fcd1@columbia.edu>
2016-05-03 13:28:58 -0400

    Add AND, OR functionality to permission check
    require_project_permission! now accepts an array of permissions
    and provides AND, OR logic
    Specs for new code uses a partial double, and config changes
    were made to spec_helper.rb to catch calls to unimplemented
    methods. An existing spec with such a call was updated.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -62,6 +62,7 @@ RSpec.configure do |config|
     #   - http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax
     expectations.syntax = :expect
   end
+=end
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
@@ -75,7 +76,6 @@ RSpec.configure do |config|
     # a real object. This is generally recommended.
     mocks.verify_partial_doubles = true
   end
-=end
 
   # Custom Hyacinth spec helper methods
 
