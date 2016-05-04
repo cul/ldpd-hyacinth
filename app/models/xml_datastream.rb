711b5758bc4d1d10d8fb0ff2d35345a7ab055367
Benjamin Armintor <armintor@gmail.com>
2016-05-04 17:42:35 -0400

    xml_datastream passes rubocop

--- app/models/xml_datastream.rb
+++ app/models/xml_datastream.rb
@@ -1,17 +1,15 @@
 class XmlDatastream < ActiveRecord::Base
-
   validate :validate_json_fields
   before_save :prettify_json
 
   def prettify_json
-    self.xml_translation = JSON.pretty_generate(JSON(self.xml_translation))
+    self.xml_translation = JSON.pretty_generate(JSON(xml_translation))
   end
 
   # Validations
   def validate_json_fields
-    if self.xml_translation.present? && ! Hyacinth::Utils::JsonUtils.valid_json?(self.xml_translation)
-      errors.add(:xml_translation, "does not validate as JSON.  Value: " + self.xml_translation.to_s)
-    end
-  end
+    return unless xml_translation.present?
 
+    errors.add(:xml_translation, "does not validate as JSON.  Value: #{xml_translation}") unless Hyacinth::Utils::JsonUtils.valid_json?(xml_translation)
+  end
 end
