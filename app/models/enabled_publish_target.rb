a15353c1620d0ae556678905379a80ed6f1941d9
Benjamin Armintor <armintor@gmail.com>
2016-05-05 13:25:50 -0400

    rubocop refactors of enabled_publish_target

--- app/models/enabled_publish_target.rb
+++ app/models/enabled_publish_target.rb
@@ -1,15 +1,13 @@
 class EnabledPublishTarget < ActiveRecord::Base
-
   belongs_to :project
   belongs_to :publish_target
 
   validates :project, :publish_target, presence: true
 
-  def as_json(options={})
-    return {
-      publish_target_pid: self.publish_target.pid,
-      publish_target_title: self.publish_target.get_title,
+  def as_json(_options = {})
+    {
+      publish_target_pid: publish_target.pid,
+      publish_target_title: publish_target.get_title
     }
   end
-
 end
