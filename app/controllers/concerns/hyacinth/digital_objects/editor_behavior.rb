021d8a87c2668cb0c8bb99306331894b78e2331d
Benjamin Armintor <armintor@gmail.com>
2016-05-13 18:43:29 -0400

    DigitalObjectsController refactored to style guidelines

---
+++ app/controllers/concerns/hyacinth/digital_objects/editor_behavior.rb
@@ -0,0 +1,56 @@
+module Hyacinth::DigitalObjects::EditorBehavior
+  def object_data_for_editor(digital_object)
+    fieldsets = Fieldset.where(project: digital_object.project)
+    enabled_dynamic_fields = digital_object.enabled_dynamic_fields
+
+    dynamic_field_hierarchy = DynamicFieldGroupCategory.all # Get all DyanamicFieldGroupCategories (which recursively includes sub-dynamic_field_groups and dynamic_fields)
+    dynamic_field_ids_to_enabled_dynamic_fields = Hash[enabled_dynamic_fields.map { |enabled_dynamic_field| [enabled_dynamic_field.dynamic_field_id, enabled_dynamic_field] }]
+
+    data_for_editor_response = {
+      digital_object: digital_object,
+      dynamic_field_hierarchy: dynamic_field_hierarchy,
+      fieldsets: fieldsets,
+      dynamic_field_ids_to_enabled_dynamic_fields: dynamic_field_ids_to_enabled_dynamic_fields,
+      allowed_publish_targets: digital_object.allowed_publish_targets.map { |pub| { display_label: pub.display_label, pid: pub.pid } }
+    }
+
+    if params['search_result_number'].present? && params['search'].present?
+      current_result_number = params['search_result_number'].to_i
+      search_params = params['search']
+
+      previous_result_pid, next_result_pid, total_num_results = DigitalObject::Base.get_previous_and_next_in_search(current_result_number, search_params, current_user)
+
+      data_for_editor_response['previous_and_next_data'] = {}
+      data_for_editor_response['previous_and_next_data']['previous_pid'] = previous_result_pid
+      data_for_editor_response['previous_and_next_data']['next_pid'] = next_result_pid
+      data_for_editor_response['previous_and_next_data']['total_num_results'] = total_num_results
+    end
+    data_for_editor_response
+  end
+
+  def ordered_children_data_for_editor(digital_object)
+    ordered_child_search_results = []
+
+    if digital_object.ordered_child_digital_object_pids.present?
+      child_pids = digital_object.ordered_child_digital_object_pids
+
+      pids_to_search_results = {}
+      search_response = DigitalObject::Base.search({ 'pids' => child_pids, 'per_page' => 99_999 }, false, current_user)
+      if search_response['results'].present?
+        search_response['results'].each do |result|
+          pids_to_search_results[result['pid']] = result
+        end
+      end
+
+      child_pids.each do |pid|
+        ordered_child_search_results.push(pids_to_search_results[pid].present? ? pids_to_search_results[pid] : { 'pid' => pid })
+      end
+    end
+
+    {
+      digital_object: digital_object,
+      ordered_child_search_results: ordered_child_search_results,
+      too_many_to_show: false # We are always showing all children.  Might change this later if this becomes a problem.
+    }
+  end
+end
--- app/models/concerns/digital_object/asset/validations.rb
+++ app/models/concerns/digital_object/asset/validations.rb
@@ -45,10 +45,20 @@ module DigitalObject::Asset::Validations
     raise "File exists, but is not readable due to a permissions issue: #{actual_path_to_validate}" unless File.readable?(actual_path_to_validate)
   end
 
-  def validate_import_file_type(import_file_import_type)
+  def admin_required_for_type?(import_file_import_type)
+    return false if DigitalObject::Asset::IMPORT_TYPE_UPLOAD_DIRECTORY == import_file_import_type
+    return false if DigitalObject::Asset::IMPORT_TYPE_POST_DATA == import_file_import_type
+    true
+  end
+
+  def validate_import_file_type(import_file_import_type, no_admin = false)
     raise "Missing type for import_file: digital_object_data['import_file']['import_type']" if import_file_import_type.blank?
 
     raise "Invalid type for import_file: digital_object_data['import_file']['import_type']: #{import_file_import_type.inspect}" unless DigitalObject::Asset::VALID_FILE_IMPORT_TYPES.include?(import_file_import_type)
+
+    return if import_file_import_type == DigitalObject::Asset::IMPORT_TYPE_UPLOAD_DIRECTORY
+
+    raise "Only admins can perform file imports of type: #{import_file_import_type}" if no_admin && admin_required_for_type?(import_file_import_type)
   end
 
   def validate_import_file_data(import_file_data)
@@ -72,5 +82,24 @@ module DigitalObject::Asset::Validations
     # Check for invalid characters in import path.  Reject if non-utf8.
     # If we get weird characters (like "\xC2"), Ruby will die a horrible death.  Let's keep Ruby alive.
     raise "Invalid UTF-8 characters found in file path.  Unable to upload." if import_file_import_path != Hyacinth::Utils::StringUtils.clean_utf8_string(import_file_import_path)
+  end
+
+  def log_import_file_data_errors_for_user(current_user, import_file_data_param, file_param)
+    data_errors = []
+    data_errors << 'Missing digital_object_data_json[import_file] for new Asset' if import_file_data_param.blank?
+
+    import_type = import_file_data_param['import_type']
+
+    begin
+      validate_import_file_type(import_type, current_user && !current_user.admin?)
+    rescue StandardError => e
+      data_errors << e.message
+    end
+
+    if import_type == DigitalObject::Asset::IMPORT_TYPE_POST_DATA
+      data_errors << "An attached file is required in the request data for imports of type: #{import_type}" if file_param.blank?
+    end
+
+    data_errors
   end
 end
