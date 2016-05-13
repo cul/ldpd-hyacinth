538cfca428f0b59a4b674c5208d186fb48e25afc
Benjamin Armintor <armintor@gmail.com>
2016-05-13 16:18:58 -0400

    Projects subresource controllers refactored to style guidelines

--- app/controllers/projects/dynamic_fields_controller.rb
+++ app/controllers/projects/dynamic_fields_controller.rb
@@ -21,22 +21,21 @@ module Projects
     end
 
     def set_contextual_nav_options
-      @contextual_nav_options['nav_title']['label'] =  '&laquo; Back to Projects'.html_safe
+      @contextual_nav_options['nav_title']['label'] = '&laquo; Back to Projects'.html_safe
       @contextual_nav_options['nav_title']['url'] = projects_path
     end
 
     private
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def project_params
-      params.require(:project).permit(
-        :id, :display_label, :string_key, :pid_generator_id, :full_path_to_custom_asset_directory,
-        :enabled_dynamic_fields_attributes => [ :id, :digital_object_type_id, :dynamic_field_id, :default_value, :required, :hidden, :locked, :_destroy, :fieldset_ids => [] ],
-        :project_permissions_attributes => [:id, :_destroy, :user_id, :can_create, :can_read, :can_update, :can_delete, :can_publish, :is_project_admin],
-        :enabled_publish_targets_attributes => [:id, :_destroy, :publish_target_id],
-        :fieldset_attributes => [:display_label, :project_id]
-      )
-    end
-
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def project_params
+        params.require(:project).permit(
+          :id, :display_label, :string_key, :pid_generator_id, :full_path_to_custom_asset_directory,
+          enabled_dynamic_fields_attributes: [:id, :digital_object_type_id, :dynamic_field_id, :default_value, :required, :hidden, :locked, :_destroy, fieldset_ids: []],
+          project_permissions_attributes: [:id, :_destroy, :user_id, :can_create, :can_read, :can_update, :can_delete, :can_publish, :is_project_admin],
+          enabled_publish_targets_attributes: [:id, :_destroy, :publish_target_id],
+          fieldset_attributes: [:display_label, :project_id]
+        )
+      end
   end
 end
--- app/controllers/projects/fieldsets_controller.rb
+++ app/controllers/projects/fieldsets_controller.rb
@@ -59,41 +59,39 @@ module Projects
         format.json { head :no_content }
       end
     end
-    
+
     private
 
-    def set_contextual_nav_options
+      def set_contextual_nav_options
+        @contextual_nav_options['nav_title']['label'] = '&laquo; Back To Fieldsets'.html_safe
 
-      @contextual_nav_options['nav_title']['label'] = '&laquo; Back To Fieldsets'.html_safe
-
-      case params[:action]
-      when 'index'
-        @contextual_nav_options['nav_title']['label'] = '&laquo; Back to Projects'.html_safe
-        @contextual_nav_options['nav_title']['url'] = projects_path
-        @contextual_nav_options['nav_items'].push(label: 'New Fieldset', url: new_project_project_fieldset_path(project_id: @project.id))
-      when 'new'
-        @contextual_nav_options['nav_title']['url'] = project_project_fieldsets_path(@project)
-      when 'edit', 'update'
-        @contextual_nav_options['nav_title']['url'] = project_project_fieldsets_path(@fieldset.project)
-        @contextual_nav_options['nav_items'].push(label: 'Delete This Fieldset',
-                                                  url: project_project_fieldset_path(project: @project, fieldset: @fieldset),
-                                                  options: {method: :delete, data: { confirm: 'Are you sure you want to delete this Fieldset?' } })
+        case params[:action]
+        when 'index'
+          @contextual_nav_options['nav_title']['label'] = '&laquo; Back to Projects'.html_safe
+          @contextual_nav_options['nav_title']['url'] = projects_path
+          @contextual_nav_options['nav_items'].push(label: 'New Fieldset', url: new_project_project_fieldset_path(project_id: @project.id))
+        when 'new'
+          @contextual_nav_options['nav_title']['url'] = project_project_fieldsets_path(@project)
+        when 'edit', 'update'
+          @contextual_nav_options['nav_title']['url'] = project_project_fieldsets_path(@fieldset.project)
+          @contextual_nav_options['nav_items'].push(label: 'Delete This Fieldset',
+                                                    url: project_project_fieldset_path(project: @project, fieldset: @fieldset),
+                                                    options: { method: :delete, data: { confirm: 'Are you sure you want to delete this Fieldset?' } })
+        end
       end
-    end
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_fieldset
-      @fieldset ||= Fieldset.find(params[:id])
-    end
-    
-    def set_project
-      @project ||= Project.find(params[:project_id])
-    end
+      # Use callbacks to share common setup or constraints between actions.
+      def set_fieldset
+        @fieldset ||= Fieldset.find(params[:id])
+      end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def fieldset_params
-      params.require(:fieldset).permit(:display_label, :project_id)
-    end
+      def set_project
+        @project ||= Project.find(params[:project_id])
+      end
 
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def fieldset_params
+        params.require(:fieldset).permit(:display_label, :project_id)
+      end
   end
 end
