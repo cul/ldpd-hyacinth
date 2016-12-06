9384fd23a110b590d8cb4a11fbe4c8bac995a09b
Fred Duby <fcd1@columbia.edu>
2016-12-06 11:20:43 -0500

    Add Ezid concern to DigitalObject::Base

---
+++ app/models/concerns/digital_object/ezid.rb
@@ -0,0 +1,52 @@
+module DigitalObject::Ezid
+  extend ActiveSupport::Concern
+  # fcd1, 11/16/16: the signature of mint_and_store_doi assumes that the shoulder will be the same for
+  # all minted EZID DOIs, so it is not passed via an argument but read straight from the EZID hash (populated
+  # via ezid.yml).
+  # returns the EZID DOI identifier string, or nil if minting did not go through
+  def mint_and_store_doi(identifier_status,
+                         target_url = nil)
+    # get the metadata from hyacinth
+    hyacinth_metadata = Hyacinth::Ezid::HyacinthMetadata.new @digital_object_data
+    # prepare the metadata into an acceptable format for EZID
+    datacite_metadata = Hyacinth::Ezid::DataciteMetadataBuilder.new hyacinth_metadata
+    # setup EZID API info: credentials, url, etc.
+    ezid_api_session = Hyacinth::Ezid::ApiSession.new(EZID[:user], EZID[:password])
+    # mint_identifier returns a Hyacinth::Ezid::Doi
+    ezid_doi_instance = ezid_api_session.mint_identifier(EZID[:shoulder][:doi],
+                                                         identifier_status,
+                                                         target_url,
+                                                         datacite: datacite_metadata.datacite_xml)
+    # if above method returned nil, call to EZID API was unsuccessful. Return nil to indicate failure
+    # if unsuccessful because of timeout, just return nil (Ezid::ApiSession#mint_identifier will log timeout).
+    return nil if ezid_api_session.timeout
+    # if got a response from EZID server indicating failure, log info and return nil
+    if ezid_doi_instance.nil?
+      response = ezid_api_session.last_response_from_server
+      Hyacinth::Utils::Logger.logger.info('EZID API call to mint_identifier was unsuccessful.')
+      Hyacinth::Utils::Logger.logger.info('Response from EZID server follows:')
+      Hyacinth::Utils::Logger.logger.info("HTTP satus code: #{response.http_status_code}")
+      Hyacinth::Utils::Logger.logger.info("HTTP server message: #{response.http_server_message}")
+      Hyacinth::Utils::Logger.logger.info("response body: #{response.body}")
+      return nil
+    end
+    @ezid_doi = ezid_doi_instance.identifier
+    # store the EZID DOI identifier in RELS-EXT of fedora object
+    @fedora_object.add_relationship(:ezid_doi, ezid_doi_instance.identifier)
+    @ezid_doi
+  end
+
+  # Following method will make a request to the EZID server to change the
+  # EZID DOI status to 'unavailable'
+  # See http://ezid.cdlib.org/doc/apidoc.html#identifier-status
+  # returns true if status change was successful
+  # if not, return false
+  def change_doi_status_to_unavailable
+    return false if @ezid_doi.nil?
+    ezid_api_session = Hyacinth::Ezid::ApiSession.new(EZID[:user], EZID[:password])
+    # ApiSession#modify_identifier returns true if the response from the EZID server indicated
+    # success, else it returns false
+    ezid_api_session.modify_identifier(@ezid_doi,
+                                       _status: Hyacinth::Ezid::Doi::IDENTIFIER_STATUS[:unavailable])
+  end
+end
---
+++ spec/models/concerns/digital_object/ezid_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+require 'equivalent-xml'
+
+describe DigitalObject::Ezid do
+
+  let(:test_class) do
+    _c = Class.new
+    _c.send :include, DigitalObject::Ezid
+  end
+
+  let(:digital_object) do
+    test_do = test_class.new 
+    data = JSON.parse( fixture('lib/hyacinth/ezid/ezid_item.json').read )
+    data['identifiers'] = ['item.' + SecureRandom.uuid] # random identifer to avoid collisions
+    test_do.instance_variable_set(:@digital_object_data, data)
+    test_do.instance_variable_set(:@fedora_object,
+                              ContentAggregator.new(pid: 'test:existingobject'))
+    test_do
+  end
+
+  context "#mint_and_store_doi:" do
+    it "mint_and_store_ezid" do
+      # stub out method wrapping API call to EZID server, stub return Doi instance
+      allow_any_instance_of(Hyacinth::Ezid::ApiSession).to receive(:mint_identifier) do
+        Hyacinth::Ezid::Doi.new('doi:10.5072/FK2F47P06D',
+                                'ark:/b5072/fk2f47p06d')
+      end
+      EZID[:user] = EZID[:ezid_test_user]
+      EZID[:password] = EZID[:ezid_test_password]
+      EZID[:shoulder][:doi] = EZID[:ezid_test_shoulder][:doi]
+      actual_ezid_doi = digital_object.mint_and_store_doi(Hyacinth::Ezid::Doi::IDENTIFIER_STATUS[:reserved],
+                                        'http://www.columbia.edu')
+      expect(actual_ezid_doi).to eq('doi:10.5072/FK2F47P06D')
+      end
+  end
+
+  context "#change_doi_status_to_unavailable:" do
+    it "change_doi_status_to_unavailable" do
+      # stub out method wrapping API call to EZID server, stub returns true (success)
+      allow_any_instance_of(Hyacinth::Ezid::ApiSession).to receive(:modify_identifier) { true }
+      EZID[:user] = EZID[:ezid_test_user]
+      EZID[:password] = EZID[:ezid_test_password]
+      EZID[:shoulder][:doi] = EZID[:ezid_test_shoulder][:doi]
+      digital_object.instance_variable_set(:@ezid_doi,
+                                           Hyacinth::Ezid::Doi.new('doi:10.5072/FK2F47P06D',
+                                                                   'ark:/b5072/fk2f47p06d') )
+      actual_return_value = digital_object.change_doi_status_to_unavailable
+      expect(actual_return_value).to eq(true)
+    end
+  end
+end
