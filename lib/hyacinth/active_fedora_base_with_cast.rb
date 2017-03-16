8e1e4d92c434114b35b330462e3447c5f3d48e2a
Eric O <eric@ejdigitalmedia.com>
2017-03-16 14:36:49 -0400

    Adapt BagAggregator to Collection when creating Hyacinth object; Safer cast with hasModel predicate check

--- lib/hyacinth/active_fedora_base_with_cast.rb
+++ lib/hyacinth/active_fedora_base_with_cast.rb
@@ -1,7 +1,8 @@
 module Hyacinth::ActiveFedoraBaseWithCast
   def self.find(pid)
     obj = ActiveFedora::Base.find(pid)
-    obj = obj.adapt_to(GenericResource) if obj.is_a?(Resource)
+    obj = obj.adapt_to(GenericResource) if obj.is_a?(Resource) && obj.relationships(:has_model).include?('info:fedora/ldpd:GenericResource')
+    obj = obj.adapt_to(Collection) if obj.is_a?(BagAggregator) && obj.relationships(:has_model).include?('info:fedora/ldpd:Collection')
     obj
   end
 end
