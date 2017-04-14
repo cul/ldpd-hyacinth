82ba82e7df24f84151affbf33a19886ae6777abf
Eric O <eric@ejdigitalmedia.com>
2017-04-14 11:34:46 -0400

    Clean whitespace to make rubocop happy

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -5,7 +5,7 @@ RESQUE_CONFIG = ActiveSupport::HashWithIndifferentAccess.new(YAML.load_file(rail
 if HYACINTH['queue_long_jobs']
   Resque.redis = RESQUE_CONFIG['url']
   Resque.redis.namespace = 'resque:' + RESQUE_CONFIG['namespace']
-  
+
   Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
   Resque.logger.level = Rails.logger.level
 end
