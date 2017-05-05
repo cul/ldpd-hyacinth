5c8f460251b5884cf6c0a1c3cfc727e5a114e64e
Eric O <eric@ejdigitalmedia.com>
2017-05-04 21:47:08 -0400

    Lower priority for DIGITAL_OBJECT_REINDEX resque queue

--- lib/hyacinth/queue.rb
+++ lib/hyacinth/queue.rb
@@ -3,7 +3,7 @@ module Hyacinth::Queue
   DIGITAL_OBJECT_CSV_EXPORT = :digital_object_csv_export
   DIGITAL_OBJECT_REINDEX = :digital_object_reindex
 
-  QUEUES_IN_DESCENDING_PRIORITY_ORDER = [DIGITAL_OBJECT_REINDEX, DIGITAL_OBJECT_CSV_EXPORT, DIGITAL_OBJECT_IMPORT]
+  QUEUES_IN_DESCENDING_PRIORITY_ORDER = [DIGITAL_OBJECT_CSV_EXPORT, DIGITAL_OBJECT_IMPORT, DIGITAL_OBJECT_REINDEX]
 
   def self.process_digital_object_import(digital_object_import_id)
     if HYACINTH['queue_long_jobs']
