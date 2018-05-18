f16dc41bef00517a39c700916cf18acfdad07cfb
Benjamin Armintor <armintor@gmail.com>
2018-05-18 13:27:51 -0400

    HYACINTH-242: add in_review and completed statuses to status enum

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -4,5 +4,5 @@ class Assignment < ActiveRecord::Base
   belongs_to :digital_object_record, class_name: 'DigitalObjectRecord'
   belongs_to :assignee, class_name: 'User'
   enum task: { annotate: 0, describe: 1, sequence: 2, transcribe: 3 }
-  enum status: { unassigned: 0, assigned: 1, in_progress: 2, ready: 3 }
+  enum status: { unassigned: 0, assigned: 1, in_progress: 2, ready: 3, in_review: 4, accepted: 5 }
 end
