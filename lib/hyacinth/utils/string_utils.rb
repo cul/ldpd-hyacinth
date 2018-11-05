95c3d5c2f86351143a1fa1da765091630c3a19b2
Benjamin Armintor <armintor@gmail.com>
2018-11-05 17:32:17 -0500

    refactor StringUtils for style conformance

--- lib/hyacinth/utils/string_utils.rb
+++ lib/hyacinth/utils/string_utils.rb
@@ -1,14 +1,14 @@
 module Hyacinth::Utils::StringUtils
   def self.ints_as_binstring(ints = [])
     stub = "".force_encoding(Encoding::ASCII_8BIT)
-    ints.inject(stub) { |m,c| m << c }
+    ints.reduce(stub) { |m, c| m << c }
   end
 
-  BOM_UTF_8 = ints_as_binstring([239,187,191])
-  BOM_UTF_16BE = ints_as_binstring([254,255])
-  BOM_UTF_16LE = ints_as_binstring([255,254])
-  BOM_UTF_32BE = ints_as_binstring([0,0,254,255])
-  BOM_UTF_32LE = ints_as_binstring([255,254,0,0])
+  BOM_UTF_8 = ints_as_binstring([239, 187, 191])
+  BOM_UTF_16BE = ints_as_binstring([254, 255])
+  BOM_UTF_16LE = ints_as_binstring([255, 254])
+  BOM_UTF_32BE = ints_as_binstring([0, 0, 254, 255])
+  BOM_UTF_32LE = ints_as_binstring([255, 254, 0, 0])
 
   def self.clean_utf8_string(str)
     # Weird character example that will fail in ruby: "\xC2".gsub('', '')
@@ -23,36 +23,63 @@ module Hyacinth::Utils::StringUtils
   end
 
   def encoded_string(source, target_encoding = Encoding::UTF_8)
-    magic_bytes = source.each_byte.lazy.first(4)
-    magic_bytes = Hyacinth::Utils::StringUtils.ints_as_binstring(magic_bytes)
-    return source if source.encoding == target_encoding
-    # these codepoint patterns indicate unicode data in an ASCII_8 string
-    case
-    when magic_bytes.start_with?(BOM_UTF_16BE)
-      source_encoding = Encoding::UTF_16BE
-      source = source.byteslice(BOM_UTF_16BE.length..-1)
-    when magic_bytes.start_with?(BOM_UTF_16LE)
-      source_encoding = Encoding::UTF_16LE
-      source = source.byteslice(BOM_UTF_16LE.length..-1)
-    when magic_bytes.start_with?(BOM_UTF_8)
-      source_encoding = Encoding::UTF_8
-      source = source.byteslice(BOM_UTF_8.length..-1)
-    when magic_bytes.start_with?(BOM_UTF_32BE)
-      source_encoding = Encoding::UTF_32BE
-      source = source.byteslice(BOM_UTF_32BE.length..-1)
-    when magic_bytes.start_with?(BOM_UTF_32LE)
-      source_encoding = Encoding::UTF_32LE
-      source = source.byteslice(BOM_UTF_32LE.length..-1)
-    else
-      detection = CharlockHolmes::EncodingDetector.detect(source)
-      source_encoding = detection[:encoding] ?
-        Encoding.find(detection[:encoding]) : Encoding::ASCII_8BIT
-    end
+    source_encoding = encoding_for_bom_indicator(source) || detected_encoding(source)
+    source = trim_bom(source, source_encoding)
     if source.encoding == target_encoding
       # just the trim, thanks
       source
     else
-      source.force_encoding(source_encoding).encode(target_encoding)
+      source = source.force_encoding(source_encoding) if source_encoding
+      source.encode(target_encoding)
     end
+  end
+
+  def encoding_for_bom_indicator(source)
+    magic_bytes = source.each_byte.lazy.first(4)
+    magic_bytes = Hyacinth::Utils::StringUtils.ints_as_binstring(magic_bytes)
+    # these codepoint patterns indicate unicode data in an ASCII_8 string
+    case
+    when magic_bytes.start_with?(BOM_UTF_16BE)
+      source_encoding = Encoding::UTF_16BE
+    when magic_bytes.start_with?(BOM_UTF_16LE)
+      source_encoding = Encoding::UTF_16LE
+    when magic_bytes.start_with?(BOM_UTF_8)
+      source_encoding = Encoding::UTF_8
+    when magic_bytes.start_with?(BOM_UTF_32BE)
+      source_encoding = Encoding::UTF_32BE
+    when magic_bytes.start_with?(BOM_UTF_32LE)
+      source_encoding = Encoding::UTF_32LE
+    end
+
+    source_encoding
+  end
+
+  def detected_encoding(source)
+    detection = CharlockHolmes::EncodingDetector.detect(source)
+    detection[:encoding] ? Encoding.find(detection[:encoding]) : Encoding::ASCII_8BIT
+  end
+
+  def bom_prefix_for(encoding)
+    case encoding
+    when Encoding::UTF_16BE
+      BOM_UTF_16BE
+    when Encoding::UTF_16LE
+      BOM_UTF_16LE
+    when Encoding::UTF_8
+      BOM_UTF_8
+    when Encoding::UTF_32BE
+      BOM_UTF_32BE
+    when Encoding::UTF_32LE
+      BOM_UTF_32LE
+    end
+  end
+
+  def trim_bom(source, encoding)
+    return source unless encoding
+    prefix = bom_prefix_for(encoding)
+    if prefix && source.byteslice(0...prefix.length).b == prefix
+      source = source.byteslice(prefix.length..-1)
+    end
+    source
   end
 end
