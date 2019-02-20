e780141b479123baaa70ac9ae8169f6f9a045794
Carla Galarza <cmg2228@columbia.edu>
2019-02-20 10:56:52 -0500

    Adding uid to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,36 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :groups
 
+  before_validation :set_uid, on: :create
+  validate :uid_unchanged
+
   def full_name
     first_name + ' ' + (middle_name || '') + ' ' + last_name
   end
 
     def as_json(_options = {})
     {
-      # uid: uid,
+      uid: uid,
       email: email,
       first_name: first_name,
       last_name: last_name,
       is_active: is_active,
     }
   end
+
+  private
+
+    def set_uid
+      if new_record?
+        self.uid = SecureRandom.uuid
+      else
+        raise StandardError, 'Cannot set uid if record has already been persisted.'
+      end
+    end
+
+    # Check that uid was not changed
+    def uid_unchanged
+      return unless persisted? # skip if object is new or is deleted
+      errors.add(:uid, 'Change of uid not allowed!') if uid_changed?
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_212949) do
+ActiveRecord::Schema.define(version: 2019_02_19_214427) do
 
   create_table "database_entry_locks", force: :cascade do |t|
     t.string "lock_key", null: false
@@ -57,8 +57,10 @@ ActiveRecord::Schema.define(version: 2019_02_18_212949) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
 end
