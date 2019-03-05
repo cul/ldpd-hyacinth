a86af9b1825883dc7f14c4ac4ef44184a157a3a2
Carla Galarza <cmg2228@columbia.edu>
2019-03-05 14:37:44 -0500

    Return appropriate response when CanCanCan::AccessDenied is raised

--- app/controllers/application_api_controller.rb
+++ app/controllers/application_api_controller.rb
@@ -4,6 +4,10 @@ class ApplicationApiController < ActionController::API
     render json: errors('Not Found'), status: :not_found
   end
 
+  rescue_from CanCan::AccessDenied do
+    render json: errors('Forbidden'), status: :forbidden
+  end
+
   private
 
     # Returns 406 status if format requested is not json. This method can be
