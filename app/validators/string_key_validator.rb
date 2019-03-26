93b83d42e33427638d34f9ba03e01030f2a9d060
Carla Galarza <cmg2228@columbia.edu>
2019-03-26 15:32:20 -0400

    Updating StringKeyValidator

--- app/validators/string_key_validator.rb
+++ app/validators/string_key_validator.rb
@@ -1,10 +1,11 @@
 class StringKeyValidator < ActiveModel::EachValidator
-  ALPHANUMERIC_UNDERSCORE_KEY_REGEX = /\A[a-z]+[a-z0-9_]*\z/
+  ALPHANUMERIC_UNDERSCORE_KEY_REGEX = /\A[a-z]{1}[a-z0-9_]{0,240}\z/
+  MESSAGE = 'values must start with a letter, can only have up to 240 characters and can only contain lower case letters, numbers and underscores'.freeze
 
   def validate_each(record, attribute, value)
     return if value.nil?
     unless value.match? ALPHANUMERIC_UNDERSCORE_KEY_REGEX
-      record.errors[attribute] << (options[:message] || "only allows lowercase alphanumeric characters and underscores and must start with a lowercase letter")
+      record.errors[attribute] << (options[:message] || MESSAGE)
     end
   end
 end
