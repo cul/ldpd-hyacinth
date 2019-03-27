0b3cdae3a3e0479e1344d58941765087d66d2f83
Carla Galarza <cmg2228@columbia.edu>
2019-03-27 11:03:22 -0400

    Rename database migration

--- db/migrate/20190325175848_create_dynamic_fields.rb
+++ db/migrate/20190325175848_create_dynamic_fields.rb
@@ -1,4 +1,4 @@
-class CreateDynamicField < ActiveRecord::Migration[5.2]
+class CreateDynamicFields < ActiveRecord::Migration[5.2]
   def change
     create_table :dynamic_fields do |t|
       t.string  :string_key,    null: false
