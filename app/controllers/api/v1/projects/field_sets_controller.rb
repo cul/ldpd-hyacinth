a6e76fd10793ced9e0255ecc121b5ea025b1f8de
Carla Galarza <cmg2228@columbia.edu>
2019-04-04 12:30:40 -0400

    Adding CRUD requests for field_sets.

---
+++ app/controllers/api/v1/projects/field_sets_controller.rb
@@ -0,0 +1,55 @@
+module Api
+  module V1
+    module Projects
+      class FieldSetsController < ApplicationApiController
+        before_action :ensure_json_request
+
+        load_resource :project, find_by: :string_key, id_param: :project_string_key
+        load_resource :field_set, through: :project
+
+        # GET /projects/:string_key/fieldsets
+        def index
+          render json: { field_sets: @project.field_sets }, status: :ok
+        end
+
+        # GET /projects/:string_key/fieldsets/:id
+        def show
+          render json: { field_set: @field_set }, status: :ok
+        end
+
+        # POST /projects/:string_key/fieldsets
+        def create
+          if @field_set.save
+            render json: { field_set: @field_set }, status: :created
+          else
+            render json: errors(@field_set.errors.full_messages), status: :unprocessable_entity
+          end
+        end
+
+        # PATCH /projects/:string_key/fieldsets/:id
+        def update
+          if @field_set.update(field_set_params)
+            render json: { fieldset: @field_set }, status: :ok
+          else
+            render json: errors(@field_set.errors.full_messages), status: :unprocessable_entity
+          end
+        end
+
+        # DELETE /projects/:string_key/fieldsets/:id
+        def destroy
+          if @field_set.destroy
+            head :no_content
+          else
+            render json: errors('Deleting was unsuccessful.'), status: :unprocessable_entity
+          end
+        end
+
+        private
+
+          def field_set_params
+            params.require(:field_set).permit(:display_label)
+          end
+      end
+    end
+  end
+end
