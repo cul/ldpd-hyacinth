fbe85644b786bbe2961d1c34b04389d7621b837d
Carla Galarza <cmg2228@columbia.edu>
2019-04-11 15:03:19 -0400

    Defining abilities using CanCanCan DSL

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,84 +1,66 @@
 class Ability
   include CanCan::Ability
 
-  # We are entirely taking over the permission calculation in order to make
-  # it as efficient as possible.
   def initialize(user)
-    can do |action, subject_class, subject|
-      action = action.to_sym
+    return if user.nil?
 
-      if user.nil?
-        false
-      elsif user.admin?
-        true
-      elsif subject_class == User
-        evaluate_user_ability(user, action, subject)
-      elsif subject_class == Group
-        evaluate_group_ability(user, action, subject)
-      elsif [Project].include? subject_class #, EnabledDynamicField, FieldSets
-        project_id = subject_class == Project ? subject.id : subject.project.id
+    if user.admin?
+      can :manage, :all
+    else
+      # Calculate Permissions
+      project_permissions = {}
+      system_permissions = []
+      user.permissions.each do |p|
+        if p.subject.blank? && p.subject_id.blank?
+          system_permissions << p.action unless system_permissions.include?(p.action)
+        elsif p.subject == Project.to_s && Permission::PROJECT_ACTIONS.include?(p.action)
+          id = p.subject_id.to_i
 
-        if read_actions.include?(action)
-          project_permissions(user, project_id).count.positive?
-        else
-          project_permissions(user, project_id).include?(:manage)
+          project_permissions[id] = []        unless project_permissions.key?(id)
+          project_permissions[id] << p.action unless project_permissions[id].include?(p.action)
         end
-      elsif subject_class == PublishTarget
-        if read_actions.include?(action)
-          project_permissions(user, subject&.project&.id).count.positive?
-        end
-      elsif subject_class == DigitalObject
-        false
       end
-    end
 
-    # TODO: This logic needs to be added above once we have a proper Project model.
-    # if subject_class == DigitalObject # TODO: NEED TO ADD TESTS
-    #   if user.system_wide_permissions.include?(PERMISSION::MANAGE_ALL_DIGITAL_OBJECTS)
-    #     true
-    #   elsif action == :read && user.system_wide_permissions.include?(PERMISSION::READ_ALL_DIGITAL_OBJECTS)
-    #     true
-    #   else
-    #     project_ids = subject.projects(&:id)
-    #     project_actions = user.available_project_actions(project_ids)
-    #
-    #     project_actions.include?('manage') || project_actions.include?("#{action}_objects")
-    #   end
-    # end
-  end
+      # Permissions all users get
+      can [:show, :update], User, id: user.id
+      can [:index, :show], Group
+      # can :index, Project, everyone should be able to see a list of projects they have access to
 
-  def project_permissions(user, project_ids)
-    allowed_actions = []
-    system_permissions = user.system_wide_permissions
-    allowed_actions << :read_object if system_permissions.include?(Permission::READ_ALL_DIGITAL_OBJECTS)
+      # System Wide Permissions
+      can :manage, User  if system_permissions.include?(Permission::MANAGE_USERS)
+      can :manage, Group if system_permissions.include?(Permission::MANAGE_GROUPS)
 
-    if system_permissions.include?(Permission::MANAGE_ALL_DIGITAL_OBJECTS)
-      allowed_actions.concat(
-        [:read_objects, :create_objects, :update_objects, :delete_objects, :publish_objects]
-      )
-    end
+      if system_permissions.include?(Permission::READ_ALL_DIGITAL_OBJECTS) || system_permissions.include?(Permission::MANAGE_ALL_DIGITAL_OBJECTS)
+        can :show, [Project, PublishTarget, FieldSet]
+      end
 
-    allowed_actions.concat(user.available_project_actions(project_ids)) if project_ids.present?
-    allowed_actions.uniq
-  end
+      # Project Based Permissions
+      project_permissions.each do |project_id, actions|
+        can :show, Project, id: project_id
+        can :show, PublishTarget, project_id: project_id
+        can :show, FieldSet, project_id: project_id
 
-  def read_actions
-    [:index, :show, :read]
-  end
+        if actions.include?('manage')
+          can :update, Project, id: project_id
+          can [:show, :create, :update, :destroy], FieldSet, project_id: project_id
+        end
+      end
 
-  def evaluate_user_ability(user, action, subject)
-    if subject && subject.id == user.id && [:edit, :update, :read, :show].include?(action)
-      true
-    else
-      user.system_wide_permissions.include?(Permission::MANAGE_USERS)
+      # Digital Object Permissions
     end
   end
 
-  def evaluate_group_ability(user, action, subject)
-    if read_actions.include?(action)
-      true
-    else
-      user.system_wide_permissions.include?(Permission::MANAGE_GROUPS)
-    end
-  end
+  # TODO: This logic needs to be added above once we have a proper Project model.
+  # if subject_class == DigitalObject # TODO: NEED TO ADD TESTS
+  #   if user.system_wide_permissions.include?(PERMISSION::MANAGE_ALL_DIGITAL_OBJECTS)
+  #     true
+  #   elsif action == :read && user.system_wide_permissions.include?(PERMISSION::READ_ALL_DIGITAL_OBJECTS)
+  #     true
+  #   else
+  #     project_ids = subject.projects(&:id)
+  #     project_actions = user.available_project_actions(project_ids)
+  #
+  #     project_actions.include?('manage') || project_actions.include?("#{action}_objects")
+  #   end
+  # end
 end
