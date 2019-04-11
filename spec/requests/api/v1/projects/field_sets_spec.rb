9261fc3c5a1a746d8ebf0c376a4cf0c4eea1feb2
Carla Galarza <cmg2228@columbia.edu>
2019-04-11 15:04:14 -0400

    Updating test to check that a user has the correct project permissions

--- spec/requests/api/v1/projects/field_sets_spec.rb
+++ spec/requests/api/v1/projects/field_sets_spec.rb
@@ -4,99 +4,128 @@ RSpec.describe 'Field Sets Requests', type: :request do
   let(:project) { FactoryBot.create(:project) }
 
   describe 'GET /api/v1/projects/:string_key/field_sets' do
-    describe 'when there are multiple results' do
-      before do
-        FactoryBot.create(:field_set, project: project)
-        FactoryBot.create(:field_set, display_label: 'Serial Part', project: project)
-        get "/api/v1/projects/#{project.string_key}/field_sets"
-      end
+    include_examples 'requires user to have correct permissions' do
+      let(:request) { get "/api/v1/projects/#{project.string_key}/field_sets" }
+    end
 
-      it 'returns all field sets' do
-        expect(response.body).to be_json_eql(%(
-          {
-            "field_sets": [
-              { "display_label": "Monographs" },
-              { "display_label": "Serial Part" }
-            ]
-          }
-        ))
-      end
+    context 'when logged in user has correct permissions' do
+      before { sign_in_project_contributor to: :read_objects, project: project }
 
-      it 'returns 200' do
-        expect(response.status).to be 200
+      describe 'when there are multiple results' do
+        before do
+          FactoryBot.create(:field_set, project: project)
+          FactoryBot.create(:field_set, display_label: 'Serial Part', project: project)
+          get "/api/v1/projects/#{project.string_key}/field_sets"
+        end
+
+        it 'returns all field sets' do
+          expect(response.body).to be_json_eql(%(
+            {
+              "field_sets": [
+                { "display_label": "Monographs" },
+                { "display_label": "Serial Part" }
+              ]
+            }
+          ))
+        end
+
+        it 'returns 200' do
+          expect(response.status).to be 200
+        end
       end
     end
   end
 
   describe 'GET /api/v1/projects/:string_key/field_sets/:id' do
-    context 'when id is valid' do
-      before do
-        field_set = FactoryBot.create(:field_set, project: project)
+    let(:field_set) { FactoryBot.create(:field_set, project: project) }
+
+    include_examples 'requires user to have correct permissions' do
+      let(:request) do
         get "/api/v1/projects/#{project.string_key}/field_sets/#{field_set.id}"
-      end
-
-      it 'returns 200' do
-        expect(response.status).to be 200
-      end
-
-      it 'returns correct response' do
-        expect(response.body).to be_json_eql(%({
-          "field_set": { "display_label": "Monographs" }
-        }))
       end
     end
 
-    context 'when id is invalid' do
-      before { get "/api/v1/projects/#{project.string_key}/field_sets/1234" }
+    context 'when logged in user has correct permissions' do
+      before { sign_in_project_contributor to: :read_objects, project: project }
 
-      it 'returns 404' do
-        expect(response.status).to be 404
+      context 'when id is valid' do
+        before do
+          get "/api/v1/projects/#{project.string_key}/field_sets/#{field_set.id}"
+        end
+
+        it 'returns 200' do
+          expect(response.status).to be 200
+        end
+
+        it 'returns correct response' do
+          expect(response.body).to be_json_eql(%({
+            "field_set": { "display_label": "Monographs" }
+          }))
+        end
       end
 
-      it 'returns correct response' do
-        expect(response.body).to be_json_eql(%(
-          { "errors": [{ "title": "Not Found" }] }
-        ))
+      context 'when id is invalid' do
+        before { get "/api/v1/projects/#{project.string_key}/field_sets/1234" }
+
+        it 'returns 404' do
+          expect(response.status).to be 404
+        end
+
+        it 'returns correct response' do
+          expect(response.body).to be_json_eql(%(
+            { "errors": [{ "title": "Not Found" }] }
+          ))
+        end
       end
     end
   end
 
   describe 'POST /api/v1/projects/:string_key/field_sets' do
-    context 'when creating a new field sets' do
-      before do
-        post "/api/v1/projects/#{project.string_key}/field_sets", params: {
-          field_set: { display_label: 'Monograph Part' }
-        }
-      end
-
-      it 'returns 201' do
-        expect(response.status).to be 201
-      end
-
-      it 'returns correct response' do
-        expect(response.body).to be_json_eql(%({
-          "field_set": { "display_label": "Monograph Part" }
-        }))
+    include_examples 'requires user to have correct permissions' do
+      let(:request) do
+        post "/api/v1/projects/#{project.string_key}/publish_targets", params: { field_set: { display_label: 'Monograph Part' } }
       end
     end
 
-    context 'when create request is missing display_label' do
-      before do
-        post "/api/v1/projects/#{project.string_key}/field_sets", params: {
-          field_set: { label: 'wrong parameter' }
-        }
+    context 'when logged in user is project manager' do
+      before { sign_in_project_contributor to: :manage, project: project }
+
+      context 'when creating a new field sets' do
+        before do
+          post "/api/v1/projects/#{project.string_key}/field_sets", params: {
+            field_set: { display_label: 'Monograph Part' }
+          }
+        end
+
+        it 'returns 201' do
+          expect(response.status).to be 201
+        end
+
+        it 'returns correct response' do
+          expect(response.body).to be_json_eql(%({
+            "field_set": { "display_label": "Monograph Part" }
+          }))
+        end
       end
 
-      it 'returns 422' do
-        expect(response.status).to be 422
-      end
+      context 'when create request is missing display_label' do
+        before do
+          post "/api/v1/projects/#{project.string_key}/field_sets", params: {
+            field_set: { label: 'wrong parameter' }
+          }
+        end
 
-      it 'returns error' do
-        expect(response.body).to be_json_eql(%({
-          "errors": [
-            { "title": "Display label can't be blank" }
-          ]
-        }))
+        it 'returns 422' do
+          expect(response.status).to be 422
+        end
+
+        it 'returns error' do
+          expect(response.body).to be_json_eql(%({
+            "errors": [
+              { "title": "Display label can't be blank" }
+            ]
+          }))
+        end
       end
     end
   end
@@ -104,38 +133,48 @@ RSpec.describe 'Field Sets Requests', type: :request do
   describe 'PATCH /api/v1/projects/:string_key/field_sets/:id' do
     let(:field_set) { FactoryBot.create(:field_set, project: project) }
 
-    context 'when updating record' do
-      before do
-        patch "/api/v1/projects/#{project.string_key}/field_sets/#{field_set.id}",
-              params: { field_set: { display_label: 'Monograph Part' } }
-      end
-
-      it 'returns 200' do
-        expect(response.status).to be 200
-      end
-
-      it 'correctly updates record' do
-        field_set.reload
-        expect(field_set.display_label).to eql 'Monograph Part'
+    include_examples 'requires user to have correct permissions' do
+      let(:request) do
+        patch "/api/v1/projects/#{project.string_key}/field_sets/#{field_set.id}", params: { field_set: {} }
       end
     end
 
-    context 'when updating record with invalid display_label' do
-      before do
-        patch "/api/v1/projects/#{project.string_key}/field_sets/#{field_set.id}",
-              params: { field_set: { display_label: nil } }
+    context 'when logged in user is project manager' do
+      before { sign_in_project_contributor to: :manage, project: project }
+
+      context 'when updating record' do
+        before do
+          patch "/api/v1/projects/#{project.string_key}/field_sets/#{field_set.id}",
+                params: { field_set: { display_label: 'Monograph Part' } }
+        end
+
+        it 'returns 200' do
+          expect(response.status).to be 200
+        end
+
+        it 'correctly updates record' do
+          field_set.reload
+          expect(field_set.display_label).to eql 'Monograph Part'
+        end
       end
 
-      it 'returns 422' do
-        expect(response.status).to be 422
-      end
+      context 'when updating record with invalid display_label' do
+        before do
+          patch "/api/v1/projects/#{project.string_key}/field_sets/#{field_set.id}",
+                params: { field_set: { display_label: nil } }
+        end
 
-      it 'returns error' do
-        expect(response.body).to be_json_eql(%({
-          "errors": [
-            { "title": "Display label can't be blank" }
-          ]
-        }))
+        it 'returns 422' do
+          expect(response.status).to be 422
+        end
+
+        it 'returns error' do
+          expect(response.body).to be_json_eql(%({
+            "errors": [
+              { "title": "Display label can't be blank" }
+            ]
+          }))
+        end
       end
     end
   end
@@ -144,33 +183,43 @@ RSpec.describe 'Field Sets Requests', type: :request do
     let(:field_set) { FactoryBot.create(:field_set, project: project) }
     let(:id)        { field_set.id }
 
-    context 'when deleting a field set that exists' do
-      before do
+    include_examples 'requires user to have correct permissions' do
+      let(:request) do
         delete "/api/v1/projects/#{project.string_key}/field_sets/#{id}"
-      end
-
-      it 'returns 204' do
-        expect(response.status).to be 204
-      end
-
-      it 'deletes record from database' do
-        expect(PublishTarget.find_by(id: id)).to be nil
       end
     end
 
-    context 'when deleting a field set that doesn\'t exist' do
-      before do
-        delete "/api/v1/projects/#{project.string_key}/field_sets/12345"
+    context 'when logged in user is project manager' do
+      before { sign_in_project_contributor to: :manage, project: project }
+
+      context 'when deleting a field set that exists' do
+        before do
+          delete "/api/v1/projects/#{project.string_key}/field_sets/#{id}"
+        end
+
+        it 'returns 204' do
+          expect(response.status).to be 204
+        end
+
+        it 'deletes record from database' do
+          expect(PublishTarget.find_by(id: id)).to be nil
+        end
       end
 
-      it 'returns errors' do
-        expect(response.body).to be_json_eql(%(
-          { "errors": [ { "title": "Not Found" } ] }
-        ))
-      end
+      context 'when deleting a field set that doesn\'t exist' do
+        before do
+          delete "/api/v1/projects/#{project.string_key}/field_sets/12345"
+        end
 
-      it 'returns 404' do
-        expect(response.status).to be 404
+        it 'returns errors' do
+          expect(response.body).to be_json_eql(%(
+            { "errors": [ { "title": "Not Found" } ] }
+          ))
+        end
+
+        it 'returns 404' do
+          expect(response.status).to be 404
+        end
       end
     end
   end
--- spec/support/authorization.rb
+++ spec/support/authorization.rb
@@ -3,19 +3,23 @@
 # access to the given request.
 shared_examples 'requires user to have correct permissions' do
   context 'when logged in user does not have appropriate permissions' do
+    include_examples 'does not have access'
+
     before do
       sign_in_user
       request
     end
+  end
+end
 
-    it 'returns 403' do
-      expect(response.status).to be 403
-    end
+shared_examples 'does not have access' do
+  it 'returns 403' do
+    expect(response.status).to be 403
+  end
 
-    it 'returns error' do
-      expect(response.body).to be_json_eql(%(
-        { "errors": [{ "title": "Forbidden" }] }
-      ))
-    end
+  it 'returns error' do
+    expect(response.body).to be_json_eql(%(
+      { "errors": [{ "title": "Forbidden" }] }
+    ))
   end
 end
