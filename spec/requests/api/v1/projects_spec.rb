bb6022b4b26e55fbce18e29de6d087f417ae480c
Carla Galarza <cmg2228@columbia.edu>
2019-05-09 14:50:30 -0400

    Adding authorization for projects endpoint

--- spec/requests/api/v1/projects_spec.rb
+++ spec/requests/api/v1/projects_spec.rb
@@ -2,15 +2,53 @@ require 'rails_helper'
 
 RSpec.describe 'Projects Requests', type: :request do
   describe 'GET /api/v1/projects' do
-    describe 'when there are multiple results' do
+    include_examples 'requires user to have correct permissions' do
+      let(:request) { get '/api/v1/projects' }
+    end
+
+    context 'when logged in user is an admin shows all projects' do
+      describe 'when there are multiple results' do
+        before do
+          sign_in_user as: :administrator
+          FactoryBot.create(:project)
+          FactoryBot.create(:project, :legend_of_lincoln)
+          get '/api/v1/projects'
+        end
+
+        it 'returns all projects' do
+          expect(response.body).to be_json_eql(%(
+            {
+              "projects": [
+                {
+                  "display_label": "Great Project",
+                  "project_url": "https://example.com/great_project",
+                  "string_key": "great_project"
+                },
+                {
+                  "display_label": "Legend of Lincoln",
+                  "project_url": "https://example.com/legend_of_lincoln",
+                  "string_key": "legend_of_lincoln"
+                }
+              ]
+            }
+          ))
+        end
+
+        it 'returns 200' do
+          expect(response.status).to be 200
+        end
+      end
+    end
+
+    context 'when logged-in user only has permissions to one project' do
       before do
-        sign_in_user as: :administrator
-        FactoryBot.create(:project)
+        project = FactoryBot.create(:project)
         FactoryBot.create(:project, :legend_of_lincoln)
+        sign_in_project_contributor to: :read_objects, project: project
         get '/api/v1/projects'
       end
 
-      it 'returns all projects' do
+      it 'returns 1 project' do
         expect(response.body).to be_json_eql(%(
           {
             "projects": [
@@ -18,11 +56,6 @@ RSpec.describe 'Projects Requests', type: :request do
                 "display_label": "Great Project",
                 "project_url": "https://example.com/great_project",
                 "string_key": "great_project"
-              },
-              {
-                "display_label": "Legend of Lincoln",
-                "project_url": "https://example.com/legend_of_lincoln",
-                "string_key": "legend_of_lincoln"
               }
             ]
           }
@@ -36,102 +69,120 @@ RSpec.describe 'Projects Requests', type: :request do
   end
 
   describe 'GET /api/v1/projects/:string_key' do
-    context 'when string_key is valid' do
-      before do
-        project = FactoryBot.create(:project)
-        get "/api/v1/projects/#{project.string_key}"
-      end
+    let(:project) { FactoryBot.create(:project) }
 
-      it 'returns 200' do
-        expect(response.status).to be 200
-      end
-
-      it 'returns correct response' do
-        expect(response.body).to be_json_eql(%({
-          "project": {
-            "display_label": "Great Project",
-            "project_url": "https://example.com/great_project",
-            "string_key": "great_project"
-          }
-        }))
-      end
+    include_examples 'requires user to have correct permissions' do
+      let(:request) { get "/api/v1/projects/#{project.string_key}" }
     end
 
-    context 'when string_key is invalid' do
-      before { get '/api/v1/projects/test-string-key' }
+    context 'when logged in user has appropriate permissions' do
+      context 'when string_key is valid' do
+        before do
+          sign_in_project_contributor to: :read_objects, project: project
+          get "/api/v1/projects/#{project.string_key}"
+        end
 
-      it 'returns 404' do
-        expect(response.status).to be 404
+        it 'returns 200' do
+          expect(response.status).to be 200
+        end
+
+        it 'returns correct response' do
+          expect(response.body).to be_json_eql(%({
+            "project": {
+              "display_label": "Great Project",
+              "project_url": "https://example.com/great_project",
+              "string_key": "great_project"
+            }
+          }))
+        end
       end
 
-      it 'returns correct response' do
-        expect(response.body).to be_json_eql(%(
-          { "errors": [{ "title": "Not Found" }] }
-        ))
+      context 'when string_key is invalid' do
+        before { get '/api/v1/projects/test-string-key' }
+
+        it 'returns 404' do
+          expect(response.status).to be 404
+        end
+
+        it 'returns correct response' do
+          expect(response.body).to be_json_eql(%(
+            { "errors": [{ "title": "Not Found" }] }
+          ))
+        end
       end
     end
   end
 
   describe 'POST /api/v1/projects' do
-    context 'when creating a new project' do
-      before do
-        post '/api/v1/projects', params: {
-          project: { string_key: 'best_project', display_label: 'Best Project', project_url: 'https://best_project.com' }
-        }
+    include_examples 'requires user to have correct permissions' do
+      let(:request) do
+        post '/api/v1/projects', params: { project: { string_key: 'best_project' } }
       end
+    end
 
-      it 'returns 201' do
-        expect(response.status).to be 201
-      end
+    context 'when logged in user has appropriate permissions' do
+      before { sign_in_user as: :administrator }
 
-      it 'returns correct response' do
-        expect(response.body).to be_json_eql(%({
-          "project": {
-            "display_label": "Best Project",
-            "project_url": "https://best_project.com",
-            "string_key": "best_project"
+      context 'when creating a new project' do
+        before do
+          post '/api/v1/projects', params: {
+            project: { string_key: 'best_project', display_label: 'Best Project', project_url: 'https://best_project.com' }
           }
-        }))
-      end
-    end
+        end
 
-    context 'when create request is missing string_key' do
-      before do
-        post '/api/v1/projects', params: {
-          project: { display_label: 'Best Project', project_url: 'https://best_project.com' }
-        }
-      end
+        it 'returns 201' do
+          expect(response.status).to be 201
+        end
 
-      it 'returns 422' do
-        expect(response.status).to be 422
-      end
-
-      it 'returns error' do
-        expect(response.body).to be_json_eql(%({
-          "errors": [
-            { "title": "String key can't be blank" }
-          ]
-        }))
-      end
-    end
-
-    context 'when create request is missing display_label' do
-      before do
-        post '/api/v1/projects', params: {
-          project: { string_key: 'best_project', project_url: 'https://best_project.com' }
-        }
+        it 'returns correct response' do
+          expect(response.body).to be_json_eql(%({
+            "project": {
+              "display_label": "Best Project",
+              "project_url": "https://best_project.com",
+              "string_key": "best_project"
+            }
+          }))
+        end
       end
 
-      it 'returns 422' do
-        expect(response.status).to be 422
+      context 'when create request is missing string_key' do
+        before do
+          post '/api/v1/projects', params: {
+            project: { display_label: 'Best Project', project_url: 'https://best_project.com' }
+          }
+        end
+
+        it 'returns 422' do
+          expect(response.status).to be 422
+        end
+
+        it 'returns error' do
+          expect(response.body).to be_json_eql(%({
+            "errors": [
+              { "title": "String key can't be blank" }
+            ]
+          }))
+        end
       end
 
-      it 'returns error' do
-        expect(response.body).to be_json_eql(%({
-          "errors": [
-            { "title": "Display label can't be blank" }
-          ]
-        }))
+      context 'when create request is missing display_label' do
+        before do
+          post '/api/v1/projects', params: {
+            project: { string_key: 'best_project', project_url: 'https://best_project.com' }
+          }
+        end
+
+        it 'returns 422' do
+          expect(response.status).to be 422
+        end
+
+        it 'returns error' do
+          expect(response.body).to be_json_eql(%({
+            "errors": [
+              { "title": "Display label can't be blank" }
+            ]
+          }))
+        end
       end
     end
   end
@@ -139,67 +190,88 @@ RSpec.describe 'Projects Requests', type: :request do
   describe 'PATCH /api/v1/projects/:string_key' do
     let(:project) { FactoryBot.create(:project) }
 
-    context 'when updating record' do
-      before do
-        patch "/api/v1/projects/#{project.string_key}", params: {
-          project: { display_label: 'Best Project', project_url: 'https://best_project.com' }
-        }
-      end
-
-      it 'returns 200' do
-        expect(response.status).to be 200
-      end
-
-      it 'correctly updates record' do
-        project.reload
-        expect(project.display_label).to eql 'Best Project'
-        expect(project.project_url).to eql 'https://best_project.com'
+    include_examples 'requires user to have correct permissions' do
+      let(:request) do
+        patch "/api/v1/projects/#{project.string_key}", params: { project: { display_label: 'Best Project' } }
       end
     end
 
-    context 'when string_key given' do
-      before do
-        patch "/api/v1/projects/#{project.string_key}", params: {
-          project: { string_key: 'best_project' }
-        }
+    context 'when logged in user has appropriate permissions' do
+      before { sign_in_project_contributor to: :manage, project: project }
+
+      context 'when updating record' do
+        before do
+          patch "/api/v1/projects/#{project.string_key}", params: {
+            project: { display_label: 'Best Project', project_url: 'https://best_project.com' }
+          }
+        end
+
+        it 'returns 200' do
+          expect(response.status).to be 200
+        end
+
+        it 'correctly updates record' do
+          project.reload
+          expect(project.display_label).to eql 'Best Project'
+          expect(project.project_url).to eql 'https://best_project.com'
+        end
       end
 
-      it 'does not update string_key' do
-        project.reload
-        expect(project.string_key).not_to eql 'best_project'
+      context 'when string_key given' do
+        before do
+          patch "/api/v1/projects/#{project.string_key}", params: {
+            project: { string_key: 'best_project' }
+          }
+        end
+
+        it 'does not update string_key' do
+          project.reload
+          expect(project.string_key).not_to eql 'best_project'
+        end
       end
     end
   end
 
   describe 'DELETE /api/v1/projects/:string_key' do
-    context 'when deleting a project that exists' do
-      let(:string_key) { 'best_project' }
+    let(:string_key) { 'best_project' }
 
-      before do
-        FactoryBot.create(:project, string_key: string_key)
+    before { FactoryBot.create(:project, string_key: string_key) }
+
+    include_examples 'requires user to have correct permissions' do
+      let(:request) do
         delete "/api/v1/projects/#{string_key}"
-      end
-
-      it 'returns 204' do
-        expect(response.status).to be 204
-      end
-
-      it 'deletes record from database' do
-        expect(Project.find_by(string_key: string_key)).to be nil
       end
     end
 
-    context 'when deleting a project that does not exist' do
-      before { delete '/api/v1/projects/not-valid' }
+    context 'when logged in user has appropriate permissions' do
+      before { sign_in_user as: :administrator }
 
-      it 'returns errors' do
-        expect(response.body).to be_json_eql(%(
-          { "errors": [ { "title": "Not Found" } ] }
-        ))
+      context 'when deleting a project that exists' do
+        before do
+          delete "/api/v1/projects/#{string_key}"
+        end
+
+        it 'returns 204' do
+          expect(response.status).to be 204
+        end
+
+        it 'deletes record from database' do
+          expect(Project.find_by(string_key: string_key)).to be nil
+        end
       end
 
-      it 'returns 404' do
-        expect(response.status).to be 404
+      context 'when deleting a project that does not exist' do
+        before { delete '/api/v1/projects/not-valid' }
+
+        it 'returns errors' do
+          expect(response.body).to be_json_eql(%(
+            { "errors": [ { "title": "Not Found" } ] }
+          ))
+        end
+
+        it 'returns 404' do
+          expect(response.status).to be 404
+        end
       end
     end
   end
