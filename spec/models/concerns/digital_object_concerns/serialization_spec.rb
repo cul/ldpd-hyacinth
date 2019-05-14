def407236619fd5a739300005df1d5a1b79d4911
Benjamin Armintor <armintor@gmail.com>
2019-05-14 16:02:23 -0400

    fix merge conflicts in serialization spec

--- spec/models/concerns/digital_object_concerns/serialization_spec.rb
+++ spec/models/concerns/digital_object_concerns/serialization_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe DigitalObjectConcerns::Serialization do
-  let(:group) { FactoryBot.create(:group) }
-
   let(:digital_object_with_sample_data) do
-    obj = FactoryBot.build(:digital_object_test_subclass, :with_sample_data, group: group)
+    obj = FactoryBot.build(:digital_object_test_subclass, :with_sample_data)
     obj.instance_variable_set :@uid, 'unique-id-123'
-    obj.group = group
     obj.resources['test_resource1'] = Hyacinth::DigitalObject::Resource.new(location: 'disk:///path/to/file1', checksum: 'SHA256:e1266b81a70083fa5e3bf456239a1160fc6ebc179cdd71e458a9dd4bc7cc21f6')
     obj.resources['test_resource2'] = Hyacinth::DigitalObject::Resource.new(location: 'disk:///path/to/file2', checksum: 'SHA256:30a7b320463d2d4a2052b72ea48518f5ad36dcb935b54628f292861241a7632e')
     obj
@@ -44,13 +41,6 @@ RSpec.describe DigitalObjectConcerns::Serialization do
       allow(digital_object_with_sample_data).to receive(:uid).and_return('unique-id-123')
       allow(digital_object_with_sample_data).to receive(:serialization_version).and_return(DigitalObject::Base::SERIALIZATION_VERSION)
       allow(digital_object_with_sample_data).to receive(:digital_object_type).and_return('test_subclass')
-
-      allow(digital_object_with_sample_data).to receive(:resource_attributes).and_return(
-        {
-          'resource1' => Hyacinth::DigitalObject::Resource.new(location: 'disk:///path/to/file1', checksum: 'SHA256:e1266b81a70083fa5e3bf456239a1160fc6ebc179cdd71e458a9dd4bc7cc21f6'),
-          'resource2' => Hyacinth::DigitalObject::Resource.new(location: 'disk:///path/to/file2', checksum: 'SHA256:30a7b320463d2d4a2052b72ea48518f5ad36dcb935b54628f292861241a7632e')
-        }
-      )
     end
     it "returns a Hash with keys for all defined metadata_attributes, and a 'resources' key with the expected value" do
       expect(digital_object_with_sample_data.to_serialized_form).to include(expected_serialized_form)
