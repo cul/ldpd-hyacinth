ca06290c5403069767b883b8f20c75560fffc163
Benjamin Armintor <armintor@gmail.com>
2019-05-20 19:58:49 -0400

    move all validators to app/validators

--- app/validators/digital_object_type_validator.rb
+++ app/validators/digital_object_type_validator.rb
@@ -1,4 +1,4 @@
-class DigitalObjectConcerns::Validations::DigitalObjectTypeValidator < ActiveModel::Validator
+class DigitalObjectTypeValidator < ActiveModel::Validator
   def validate(digital_object)
     return if Hyacinth.config.digital_object_types.include?(digital_object.digital_object_type)
     digital_object.errors[:digital_object_type] << "Unregistered digital object type #{digital_object.digital_object_type}"
