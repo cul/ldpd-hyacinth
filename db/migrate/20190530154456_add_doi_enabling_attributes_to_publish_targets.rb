45fc12d17cfbba5ae33f40c97f169092667f8b19
Benjamin Armintor <armintor@gmail.com>
2019-06-03 10:21:33 -0400

    clarify name of publish target migration for DOI enabling fields

--- db/migrate/20190530154456_add_doi_enabling_attributes_to_publish_targets.rb
+++ db/migrate/20190530154456_add_doi_enabling_attributes_to_publish_targets.rb
@@ -1,4 +1,4 @@
-class AddDoiLocationToPublishTargets < ActiveRecord::Migration[5.2]
+class AddDoiEnablingAttributesToPublishTargets < ActiveRecord::Migration[5.2]
   def change
     add_column :publish_targets, :is_allowed_doi_target, :boolean, null: false, default: false
     add_column :publish_targets, :doi_priority, :integer, null: false, default: 0
