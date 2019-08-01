8858f7f9f815204cde3f3ab0bf191adb6f32de93
Carla Galarza <cmg2228@columbia.edu>
2019-08-01 17:08:58 -0400

    Converting symbols to CamelCase inorder to match class names.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -73,7 +73,7 @@ class Ability
 
   def to_list
     rules.map do |rule|
-      object = { actions: rule.actions, subject: rule.subjects.map { |s| s.is_a?(Symbol) ? s : s.name } }
+      object = { actions: rule.actions, subject: rule.subjects.map { |s| s.is_a?(Symbol) ? s.to_s.camelcase : s.name } }
       object[:conditions] = rule.conditions unless rule.conditions.blank?
       object[:inverted] = true unless rule.base_behavior
       object
