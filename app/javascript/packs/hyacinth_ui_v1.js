f30804c2dfe4cabffd4f49a7c63536ed09bc11f8
Carla Galarza <cmg2228@columbia.edu>
2019-09-26 12:43:30 -0400

    Adding Apollo to our React client in order to make graphql queries

--- app/javascript/packs/hyacinth_ui_v1.js
+++ app/javascript/packs/hyacinth_ui_v1.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import Constants from '../hyacinth_ui_v1/Constants';
 
@@ -12,10 +14,18 @@ import '../hyacinth_ui_v1/stylesheets/hyacinth_ui_v1.scss';
 // add app-wide support for FontAwesome
 import '../hyacinth_ui_v1/util/FontAwesome';
 
+
+const client = new ApolloClient({
+  uri: '/graphql',
+});
+
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Router basename={Constants.APPLICATION_BASE_PATH}>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </Router>,
     document.getElementById('hyacinth-ui-v1-app'),
   );
