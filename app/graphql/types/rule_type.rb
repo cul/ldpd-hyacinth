5e63901f3d58bad6fa1c6fbf8bf942412e3b94ca
Carla Galarza <cmg2228@columbia.edu>
2019-10-23 12:02:45 -0400

    Using graphql query to load logged in user

--- app/graphql/types/rule_type.rb
+++ app/graphql/types/rule_type.rb
@@ -1,7 +1,7 @@
 module Types
   class RuleType < Types::BaseObject
     description 'A CanCan Rule'
-    field :action, String, null: false
+    field :actions, [String], null: false
     field :subject, [String], null: true
     field :conditions, String, null: true
     field :inverted, String, null: true
