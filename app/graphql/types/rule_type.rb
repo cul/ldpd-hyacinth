632e0d65d4e847884d56d98999bdc78ff279b77a
Carla Galarza <cmg2228@columbia.edu>
2019-10-29 17:31:24 -0400

    Defining conditions as JSON type

--- app/graphql/types/rule_type.rb
+++ app/graphql/types/rule_type.rb
@@ -3,7 +3,7 @@ module Types
     description 'A CanCan Rule'
     field :actions, [String], null: false
     field :subject, [String], null: true
-    field :conditions, String, null: true
+    field :conditions, GraphQL::Types::JSON, null: true
     field :inverted, String, null: true
   end
 end
