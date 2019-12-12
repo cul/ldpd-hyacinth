b56832e8e6c8b3ae5a4c88d0405fc6a0ac3cad68
Carla Galarza <cmg2228@columbia.edu>
2019-12-12 13:26:11 -0500

    Fixing enabled fields pages to use ProjectInterface component

--- app/javascript/hyacinth_ui_v1/components/projects/enabled_dynamic_fields/EnabledDynamicFieldEdit.js
+++ app/javascript/hyacinth_ui_v1/components/projects/enabled_dynamic_fields/EnabledDynamicFieldEdit.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import { startCase } from 'lodash';
+import produce from 'immer';
 
 import TabHeading from '../../ui/tabs/TabHeading';
 import EnabledDynamicFieldForm from './EnabledDynamicFieldForm';
+import { projects } from '../../../util/hyacinth_api';
+import ProjectInterface from '../ProjectInterface';
 
-export default class EnabledDynamicFieldsEdit extends React.PureComponent {
+export default class EnabledDynamicFieldsEdit extends React.Component {
+  state = {
+    project: {},
+  }
+
+  componentDidMount() {
+    const { match: { params: { projectStringKey } } } = this.props;
+
+    projects.get(projectStringKey)
+      .then((res) => {
+        const { project } = res.data;
+
+        this.setState(produce((draft) => {
+          draft.project = project;
+        }));
+      });
+  }
+
   render() {
     const { match: { params: { projectStringKey, digitalObjectType } } } = this.props;
 
     return (
-      <>
+      <ProjectInterface project={this.state.project} key={digitalObjectType}>
         <TabHeading>{`Edit Enabled Dynamic Fields for ${startCase(digitalObjectType)}`}</TabHeading>
         <EnabledDynamicFieldForm
           formType="edit"
           projectStringKey={projectStringKey}
           digitalObjectType={digitalObjectType}
         />
-      </>
+      </ProjectInterface>
     );
   }
 }
