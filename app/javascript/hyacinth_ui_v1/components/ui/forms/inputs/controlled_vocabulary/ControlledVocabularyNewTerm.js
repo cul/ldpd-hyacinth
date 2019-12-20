be8aa85d84a503dfd1a2860850e7fb8f1b45b7d2
Carla Galarza <cmg2228@columbia.edu>
2019-12-20 16:42:26 -0500

    Seperating out option into its own component

--- app/javascript/hyacinth_ui_v1/components/ui/forms/inputs/controlled_vocabulary/ControlledVocabularyNewTerm.js
+++ app/javascript/hyacinth_ui_v1/components/ui/forms/inputs/controlled_vocabulary/ControlledVocabularyNewTerm.js
@@ -3,14 +3,14 @@ import React from 'react';
 import TermForm from '../../../../controlled_vocabularies/terms/TermForm';
 
 function ControlledVocabularyNewTerm(props) {
-  const { vocabulary, onChange, displayNewTerm } = props;
+  const { vocabulary, onChange, displayNewTerm, close } = props;
 
   return (
     <div className="px-3">
       <TermForm
         formType="new"
         vocabulary={vocabulary}
-        submitAction={(term) => { onChange(term); displayNewTerm(false); }}
+        submitAction={(term) => { onChange(term); displayNewTerm(false); close(); }}
         cancelAction={() => displayNewTerm(false)}
         small
       />
