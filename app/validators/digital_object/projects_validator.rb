7401b303ff063c3c3b05d58fea86ae074bee8600
Benjamin Armintor <armintor@gmail.com>
2020-01-02 12:03:42 -0500

    organize digital object validators separately from general type validators

--- app/validators/digital_object/projects_validator.rb
+++ app/validators/digital_object/projects_validator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ProjectsValidator < ActiveModel::Validator
+class DigitalObject::ProjectsValidator < ActiveModel::Validator
   def validate(digital_object)
     digital_object.errors[:projects] << "Only primary projects can be assigned to primary_project" if digital_object.primary_project && !digital_object.primary_project.is_primary
     return unless digital_object.other_projects.detect(&:is_primary)
