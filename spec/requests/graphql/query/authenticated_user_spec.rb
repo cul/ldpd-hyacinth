dc3ea24d4f83131d0320d326a0c6be10c14aaf62
Carla Galarza <cmg2228@columbia.edu>
2020-01-02 15:34:11 -0500

    Adding test for authenticated_user

---
+++ spec/requests/graphql/query/authenticated_user_spec.rb
@@ -0,0 +1,218 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Query for Authenticated User', type: :request do
+  context 'when user without additional permissions is logged in' do
+    before do
+      sign_in_user
+      graphql query
+    end
+
+    let(:expected_rules) do
+      %(
+        [
+          {
+            "actions": ["read", "update"],
+            "conditions": {
+              "id": #{User.first.id}
+            },
+            "inverted": false,
+            "subject": ["User"]
+          },
+          {
+            "actions": ["read", "update"],
+            "conditions": {
+              "uid": "#{User.first.uid}"
+            },
+            "inverted": false,
+            "subject": ["User"]
+          },
+          {
+            "actions": ["read", "create"],
+            "conditions": {},
+            "inverted": false,
+            "subject": ["Term"]
+          },
+          {
+            "actions": ["read"],
+            "conditions": {},
+            "inverted": false,
+            "subject": ["DynamicFieldCategory"]
+          }
+        ]
+       )
+    end
+
+    it 'returns expected rules' do
+      expect(response.body).to be_json_eql(expected_rules).at_path('data/authenticatedUser/rules').including('id')
+    end
+  end
+
+  context 'when project user is logged in' do
+    let(:project) { FactoryBot.create(:project) }
+    before do
+      sign_in_project_contributor to: [:read_objects, :update_objects, :assess_rights], project: project
+      graphql query
+    end
+
+    let(:expected_rules) do
+      %(
+        [
+          {
+            "actions": ["read", "update"],
+            "conditions": {
+              "id": #{User.first.id}
+            },
+            "inverted": false,
+            "subject": ["User"]
+          },
+          {
+            "actions": ["read", "update"],
+            "conditions": {
+              "uid": "#{User.first.uid}"
+            },
+            "inverted": false,
+            "subject": ["User"]
+          },
+          {
+            "actions": ["read", "create"],
+            "conditions": {},
+            "inverted": false,
+            "subject": ["Term"]
+          },
+          {
+            "actions": ["read"],
+            "conditions": {},
+            "inverted": false,
+            "subject": ["DynamicFieldCategory"]
+          },
+          {
+            "actions": ["read"],
+            "conditions": {
+              "id": #{project.id}
+            },
+            "inverted": false,
+            "subject": ["Project"]
+          },
+          {
+            "actions": ["read"],
+            "conditions": {
+              "stringKey": "great_project"
+            },
+            "inverted": false,
+            "subject": ["Project"]
+          },
+          {
+            "actions": ["read"],
+            "conditions": {
+              "projectId": 1
+            },
+            "inverted": false,
+            "subject": ["PublishTarget"]
+          },
+          {
+            "actions": ["read"],
+            "conditions": {
+              "project": {
+                "stringKey": "great_project"
+              }
+            },
+            "inverted": false,
+            "subject": ["PublishTarget"]
+          },
+          {
+            "actions": ["read"],
+            "conditions": {
+              "projectId": 1
+            },
+            "inverted": false,
+            "subject": ["FieldSet"]
+          },
+          {
+            "actions": ["read"],
+            "conditions": {
+              "project": {
+                "stringKey": "great_project"
+              }
+            },
+            "inverted": false,
+            "subject": ["FieldSet"]
+          },
+          {
+            "actions": ["read_objects"],
+            "conditions": {
+              "id": #{project.id}
+            },
+            "inverted": false,
+            "subject": ["Project"]
+          },
+          {
+            "actions": ["read_objects"],
+            "conditions": {
+              "stringKey": "great_project"
+            },
+            "inverted": false,
+            "subject": ["Project"]
+          },
+          {
+            "actions": ["update_objects"],
+            "conditions": {
+              "id": #{project.id}
+            },
+            "inverted": false,
+            "subject": ["Project"]
+          },
+          {
+            "actions": ["update_objects"],
+            "conditions": {
+              "stringKey": "great_project"
+            },
+            "inverted": false,
+            "subject": ["Project"]
+          },
+          {
+            "actions": ["assess_rights"],
+            "conditions": {
+              "id": #{project.id}
+            },
+            "inverted": false,
+            "subject": ["Project"]
+          },
+          {
+            "actions": ["assess_rights"],
+            "conditions": {
+              "stringKey": "great_project"
+            },
+            "inverted": false,
+            "subject": ["Project"]
+          }
+        ]
+      )
+    end
+
+    it 'returns expected rules' do
+      expect(response.body).to be_json_eql(expected_rules).at_path('data/authenticatedUser/rules').including('id')
+    end
+  end
+
+  def query
+    <<~GQL
+      query {
+        authenticatedUser {
+          email
+          firstName
+          lastName
+          isActive
+          isAdmin
+          rules {
+            actions
+            subject
+            conditions
+            inverted
+          }
+        }
+      }
+    GQL
+  end
+end
