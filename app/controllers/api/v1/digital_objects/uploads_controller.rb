22b182b8682ad79d75d6a862c0ebe75c07c57d2e
Benjamin Armintor <armintor@gmail.com>
2020-01-13 13:55:29 -0500

    REST resource under digital objects api to create ActiveStorage blobs (HYACINTH-418)

---
+++ app/controllers/api/v1/digital_objects/uploads_controller.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module DigitalObjects
+      class UploadsController < ApplicationApiController
+        before_action :ensure_json_request
+        before_action :load_resource, only: [:create]
+
+        # POST /digital_objects/1/uploads
+        def create
+          authorize! :update, @digital_object
+          blob = ActiveStorage::Blob.create_before_direct_upload!(**create_params)
+
+          # this is a development hack to alow us to get the path despite ActiveStorage's protests
+          ActiveStorage::Current.host = 'stubhost'
+
+          render json: blob.as_json(root: false, methods: :signed_id).merge(direct_upload: {
+            url: blob.service_url_for_direct_upload.sub("http://#{ActiveStorage::Current.host}", ''),
+            headers: blob.service_headers_for_direct_upload
+          })
+        end
+
+        def load_resource
+          @digital_object ||= DigitalObject::Base.find(params[:id])
+        end
+
+        private
+
+          def create_params
+            # TODO: decide how we want to validate rights parameters
+            params.require(:blob).permit(:filename, :byte_size, :checksum, :content_type, :metadata).to_h.symbolize_keys
+          end
+      end
+    end
+  end
+end
