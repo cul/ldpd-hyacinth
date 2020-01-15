401f837b954b21201e9089b2420e1c9efcba0283
Eric O <eric@ejdigitalmedia.com>
2020-01-15 10:54:19 -0500

    Move defaultItemRights function into separate module

--- app/javascript/hyacinth_ui_v1/components/digital_objects/rights/rights_form/ItemRightsForm.js
+++ app/javascript/hyacinth_ui_v1/components/digital_objects/rights/rights_form/ItemRightsForm.js
@@ -19,107 +19,14 @@ import LicensedToColumbiaUniversity from './subsections/LicensedToColumbiaUniver
 import ContractualLimitationsRestrictionsAndPermissions from './subsections/ContractualLimitationsRestrictionsAndPermissions';
 import RightsForWorksOfArtSculptureAndPhotographs from './subsections/RightsForWorksOfArtSculptureAndPhotographs';
 import UnderlyingRights from './subsections/UnderlyingRights';
+import defaultItemRights from './defaultItemRights';
 
 function ItemRightsForm(props) {
   const { digitalObject } = props;
   const { id, rights: initialRights, dynamicFieldData } = digitalObject;
   const history = useHistory();
 
-  const defaultRights = () => ({
-    descriptiveMetadata: {
-      typeOfContent: '',
-      countryOfOrigin: {},
-      filmDistributedToPublic: '',
-      filmDistributedCommercially: '',
-    },
-    copyrightStatus: {
-      copyrightStatement: '',
-      copyrightNote: [''],
-      copyrightRegistered: '',
-      copyrightRenewed: '',
-      copyrightDateOfRenewal: '',
-      copyrightExpirationDate: '',
-      culCopyrightAssessmentDate: '',
-    },
-    additionalRightsToRecord: {
-      enabled: false,
-    },
-    copyrightOwnership: {
-      enabled: false,
-      copyrightOwners: [
-        {
-          name: {},
-          heirs: '',
-          contactInformation: '',
-        },
-      ],
-    },
-    columbiaUniversityIsCopyrightHolder: {
-      enabled: false,
-      dateOfTransfer: '',
-      dateOfExpiration: '',
-      transferDocumentionEnabled: false,
-      transferDocumentation: '',
-      otherTransferEvidenceEnabled: false,
-      otherTransferEvidence: '',
-      transferDocumentationNote: '',
-    },
-    licensedToColumbiaUniversity: {
-      enabled: false,
-      dateOfLicense: '',
-      terminationDateOfLicense: '',
-      credits: '',
-      acknowledgements: '',
-      licenseDocumentationLocation: '',
-    },
-    contractualLimitationsRestrictionsAndPermissions: {
-      a: false,
-      b: false,
-      c: false,
-      d: false,
-      e: false,
-      avA: false,
-      avB: false,
-      avC: false,
-      avD: false,
-      avE: false,
-      avF: false,
-      avG: false,
-      enabled: false,
-      reproductionAndDistributionProhibitedUntil: '',
-      photoGraphicOrFilmCredit: '',
-      excerptLimitedTo: '',
-      other: '',
-      permissionsGrantedAsPartOfTheUseLicenseEnabled: false,
-      permissionsGrantedAsPartOfTheUseLicense: [],
-    },
-    rightsForWorksOfArtSculptureAndPhotographs: {
-      enabled: false,
-      publicityRightsPresentEnabled: false,
-      publicityRightsPresent: '',
-      trademarksProminentlyVisible: '',
-      sensitiveInNature: '',
-      privacyConcerns: '',
-      childrenMateriallyIdentifiableInWork: '',
-      varaRightsConcerns: '',
-      note: '',
-    },
-    underlyingRights: {
-      enabled: false,
-      doWeKnowSpecificUnderlyingRightsEnabled: false,
-      note: '',
-      musicRightsEnabled: false,
-      talentRights: '',
-      musicLicensedToColumbiaEnabled: false,
-      columbiaMusicLicense: '',
-      composition: '',
-      recording: '',
-      otherUnderlyingRights: [],
-      other: '',
-    },
-  });
-
-  const [rights, setRights] = useState(merge({}, defaultRights(), initialRights));
+  const [rights, setRights] = useState(merge({}, defaultItemRights(), initialRights));
 
   const onChange = (subsection, value) => {
     setRights(produce(rights, (draft) => {
