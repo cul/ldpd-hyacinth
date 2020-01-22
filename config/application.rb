0b0aefbcabbf4f4785723fe98675842b25859d22
Eric O <eric@ejdigitalmedia.com>
2020-01-22 12:26:59 -0500

    Update various config files for Rails 6; Update bin/setup for Rails 6

--- config/application.rb
+++ config/application.rb
@@ -11,16 +11,18 @@ Bundler.require(*Rails.groups)
 module Hyacinth
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Custom directories with classes and modules you want to be eager loaded.
-    # In Rails 5, production environments ignore autoload_paths, so eager_load_paths is the way to go.
-    config.eager_load_paths += %W(#{config.root}/lib)
+    config.eager_load_paths += %W[#{config.root}/lib]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Don't generate system test files.
+    config.generators.system_tests = true
 
     config.middleware.use OliveBranch::Middleware
   end
