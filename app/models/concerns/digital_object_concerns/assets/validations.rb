69c7c52f141e9cd37a8e88da843b9c5cffe69b81
Benjamin Armintor <armintor@gmail.com>
2020-01-29 12:48:26 -0500

    DigitalObject::Asset validations confirm acceptability of rights assessment (HYACINTH-483)

--- app/models/concerns/digital_object_concerns/assets/validations.rb
+++ app/models/concerns/digital_object_concerns/assets/validations.rb
@@ -5,9 +5,15 @@ module DigitalObjectConcerns::Assets::Validations
 
   included do
     validate :validate_type
+    validate :validate_rights_updates
   end
 
   def validate_type
     errors.add(:asset_type, "Invalid asset type: #{asset_type}") unless BestType.pcdm_type.valid_type?(asset_type)
+  end
+
+  def validate_rights_updates
+    return unless rights.keys.detect { |key| rights[key].present? && !'restriction_on_access'.eql?(key.to_s) }
+    errors.add(:rights, "Asset-level rights assessment not enabled in #{primary_project.display_label}") unless primary_project.has_asset_rights
   end
 end
