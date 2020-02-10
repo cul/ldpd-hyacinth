223fc30571c8cea114bd5da6c92214b1d5a785ab
Carla Galarza <cmg2228@columbia.edu>
2020-02-10 14:17:17 -0500

    Changing definition of a blank field to include False values

--- lib/hyacinth/utils/clean.rb
+++ lib/hyacinth/utils/clean.rb
@@ -3,16 +3,15 @@
 module Hyacinth
   module Utils
     module Clean
-      # Recursively trims whitespace from strings and removes any non-boolean blank
-      # values (aka non-boolean values that return true for .blank?).
+      # Recursively trims whitespace from strings and removes any blank
+      # values (aka values that return true for .blank?).
       def self.trim_whitespace_and_remove_blank_fields!(data)
         trim_whitespace!(data)
         remove_blank_fields!(data)
       end
 
       # Recursively removes blank fields from the Arrays and Hashes. Blank values are any values that
-      # return true to `#blank?` with the exception of booleans. All booleans are not considered to be blank.
-      # This method is destructive and updates directly the object that is given.
+      # return true to `#blank?`. This method is destructive and updates directly the object that is given.
       #
       # @param data [Hash|Array] data
       def self.remove_blank_fields!(data)
@@ -22,10 +21,10 @@ module Hyacinth
         # Step 2: Delete blank values on this object level
         if data.is_a?(Hash)
           data.each { |_, v| remove_blank_fields!(v) }
-          data.delete_if { |_key, value| blank_field?(value) }
+          data.delete_if { |_key, value| value.blank? }
         elsif data.is_a?(Array)
           data.each { |v| remove_blank_fields!(v) }
-          data.delete_if { |v| blank_field?(v) }
+          data.delete_if(&:blank?)
         end
       end
 
@@ -51,11 +50,6 @@ module Hyacinth
             end
           end
         end
-      end
-
-      # Wrapper around '.blank?' method because any boolean value should not be considered blank.
-      def self.blank_field?(value)
-        value.is_a?(FalseClass) ? false : value.blank?
       end
     end
   end
