e7c4125a7dbb87b59795f782e5e3c4d69bbdd2f2
Carla Galarza <cmg2228@columbia.edu>
2020-02-20 15:39:15 -0500

    Adding tests for path methods

--- app/models/concerns/dynamic_field_structure/path.rb
+++ app/models/concerns/dynamic_field_structure/path.rb
@@ -4,15 +4,18 @@ module DynamicFieldStructure
   module Path
     extend ActiveSupport::Concern
 
-    def path
-      collect_path([], self)
+    class_methods do
+      def collect_path(path, group_or_category)
+        return path if group_or_category.class == DynamicFieldCategory
+
+        raise ArgumentError, 'Must respond to #parent in order to collect path' unless group_or_category.respond_to?(:parent)
+        parent = group_or_category.parent
+        collect_path(path.unshift(parent), parent)
+      end
     end
 
-    def collect_path(path, group_or_category)
-      return path if group_or_category.class == DynamicFieldCategory
-
-      parent = group_or_category.parent
-      collect_path(path.unshift(parent), parent)
+    def path
+      self.class.collect_path([], self)
     end
   end
 end
