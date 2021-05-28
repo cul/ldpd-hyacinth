336830d0ed54e30df306ad63d46fa7d95c0c98e0
JackBlackLight <jd2148@columbia.edu>
2021-05-28 14:59:44 -0400

    moves enabled field checks to module

--- app/validators/digital_object/descriptive_fields_validator.rb
+++ app/validators/digital_object/descriptive_fields_validator.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 class DigitalObject::DescriptiveFieldsValidator < DigitalObject::DynamicFieldsValidator
+  include DigitalObject::EnabledDynamicFieldsValidations
+
   def validate_each(digital_object, attribute, value)
     return if value.blank?
 
+    enabled_field_errors(digital_object, value).each do |a|
+      digital_object.errors.add(a[0], a[1])
+    end
+
     map = Hyacinth::DynamicFieldsMap.new('descriptive').map
 
-    generate_errors(digital_object, attribute, value, map, true)
+    generate_errors(digital_object, attribute, value, map)
   end
 end
---
+++ app/validators/digital_object/enabled_dynamic_fields_validations.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module DigitalObject::EnabledDynamicFieldsValidations
+  # Returns any errors related to project level validations.
+  #
+  # @return [String] if there are errors
+  # @return false if there are no errors
+  def enabled_field_errors(digital_object, data)
+    errors = []
+    dynamic_field_paths = []
+    data.each do |df_group, children|
+      dynamic_field_paths = collect_field_paths(df_group, children, dynamic_field_paths)
+
+      dynamic_field_paths.each do |dynamic_field_path|
+        if (e = disabled?(dynamic_field_path, digital_object))
+          errors.concat e.map { |i| [dynamic_field_path, i] }
+        end
+      end
+    end
+
+    errors = check_required_fields(dynamic_field_paths, digital_object, errors)
+
+    errors
+  end
+
+  def disabled?(dynamic_field_path, digital_object)
+    no_match = true
+    dynamic_field = DynamicField.find_by(path: dynamic_field_path)
+    if dynamic_field && EnabledDynamicField.where(dynamic_field: dynamic_field.id,
+                                project: digital_object.primary_project,
+                                digital_object_type: digital_object.digital_object_type).first
+      no_match = false
+    end
+    return ['field must be enabled'] if no_match
+    false
+  end
+
+  def check_required_fields(dynamic_field_paths, digital_object, errors)
+    required_enabled_fields = EnabledDynamicField.where(project: digital_object.primary_project,
+                                  digital_object_type: digital_object.digital_object_type, required: true)
+
+    required_enabled_fields.each do |enabled_field|
+      errors.concat ['is required'].map { |i| [enabled_field.dynamic_field.path, i] } unless dynamic_field_paths.include?(enabled_field.dynamic_field.path)
+    end
+
+    errors
+  end
+
+  def collect_field_paths(df_group, children, df_paths)
+    children.each do |child|
+      child.each do |df_name, value|
+        df_paths << "#{df_group}/#{df_name}" if value
+      end
+    end
+    df_paths
+  end
+end
