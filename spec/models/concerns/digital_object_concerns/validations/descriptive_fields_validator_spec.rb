670dc5aa546fceb19e9b5a0914e9b843011c2d1f
Eric O <eric@ejdigitalmedia.com>
2021-07-19 17:53:49 -0400

    Update ruby date field validation to use EDTF library

--- spec/models/concerns/digital_object_concerns/validations/descriptive_fields_validator_spec.rb
+++ spec/models/concerns/digital_object_concerns/validations/descriptive_fields_validator_spec.rb
@@ -257,12 +257,12 @@ RSpec.describe DigitalObject::DescriptiveFieldsValidator do
     end
   end
 
-  context 'with valid dates of varying specificity, including dates with question marks indicating uncertainty', focus: true do
+  context 'with valid EDTF dates of varying specificity' do
     [
       '1900-01-01', '1900-01', '1900',
       '-1900-01-01', '-1900-01', '-1900',
-      '190?', '19??', '1???', '000?', '00??', '0???',
-      '-190?', '-19??', '-1???'
+      '1900-01-01', '1900-01', '1900',
+      '190X', '19XX', '000X', '00XX'
     ].each do |date|
       context "date = #{date}" do
         let(:descriptive_metadata) do
@@ -298,7 +298,7 @@ RSpec.describe DigitalObject::DescriptiveFieldsValidator do
     it 'return errors' do
       expect(item.valid?).to be false
       expect(item.errors.messages).to include(
-        'descriptive_metadata.date_created[0].start_date': ['must be in YYYY-MM-DD format (with optional leading negative sign for BCE dates)']
+        'descriptive_metadata.date_created[0].start_date': ['must be a valid EDTF date']
       )
     end
   end
@@ -311,7 +311,7 @@ RSpec.describe DigitalObject::DescriptiveFieldsValidator do
     it 'return errors' do
       expect(item.valid?).to be false
       expect(item.errors.messages).to include(
-        'descriptive_metadata.date_created[0].start_date': ['is an invalid date']
+        'descriptive_metadata.date_created[0].start_date': ['must be a valid EDTF date']
       )
     end
   end
