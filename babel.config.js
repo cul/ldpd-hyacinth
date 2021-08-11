329fd06f2b0821892c439499be5e9d40cd184155
Eric O <eric@ejdigitalmedia.com>
2021-08-11 10:31:42 -0400

    package.json fix for production webpack build (Rails setup requires some babel packages in dependencies section instead of devDependencies)

--- babel.config.js
+++ babel.config.js
@@ -48,6 +48,7 @@ module.exports = function (api) {
     plugins: [
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
