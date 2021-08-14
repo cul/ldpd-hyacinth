208f23459dfc6d51bbfb8e5ca0c1fec84db9062b
Benjamin Armintor <armintor@gmail.com>
2021-08-14 09:16:06 -0400

    use splitChunks (HYACINTH-853)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,3 @@
 const { environment } = require('@rails/webpacker');
 
-const environmentConfig = environment.config;
-const outputConfig = environmentConfig.get('output');
-const { fileName } = outputConfig;
-outputConfig.chunkFilename = fileName;
-
 module.exports = environment;
