669beef10dc65bb956a0cf7c987c209fef15b682
Benjamin Armintor <armintor@gmail.com>
2021-09-08 09:35:01 -0400

    remove solr flag from graphql schema tests and resolve spec TODOs in delete_resource_spec.rb

--- spec/graphql/hyacinth_schema_spec.rb
+++ spec/graphql/hyacinth_schema_spec.rb
@@ -2,18 +2,14 @@
 
 require 'rails_helper'
 
-RSpec.describe HyacinthSchema, solr: true do
-  let(:graphql_request) { HyacinthSchema.execute(query, context: context, variables: variables) }
+RSpec.describe HyacinthSchema do
+  let(:graphql_request) { described_class.execute(query, context: context, variables: variables) }
   let(:context) { { current_user: FactoryBot.create(:user), ability: ability } }
-  let(:ability) do
-    abil = double
-    # skipping authorization because it's not part of what we're testing
-    allow(abil).to receive(:authorize!)
-    abil
-  end
+  let(:ability) { instance_double(Ability) }
   let(:graphql_response_as_hash) { graphql_request.to_h }
 
   describe 'schema-level error rescue' do
+    include_context 'with stubbed search adapters'
     let(:digital_object) { FactoryBot.create(:item) }
     let(:query) do
       <<~GQL
@@ -30,9 +26,16 @@ RSpec.describe HyacinthSchema, solr: true do
     let(:variables) { { input: { id: digital_object.uid, descriptiveMetadata: { title: [{ non_sort_portion: "The", sort_portion: "Text Here Does Not Matter" }] } } } }
 
     context "when a Hyacinth::Exceptions::HyacinthError is raised during a query" do
-      before { allow_any_instance_of(Mutations::DigitalObject::UpdateDescriptiveMetadata).to receive(:resolve).and_raise(Hyacinth::Exceptions::HyacinthError, 'This is the error') }
+      let(:query_class) { Mutations::DigitalObject::UpdateDescriptiveMetadata }
+      let(:error_class) { Hyacinth::Exceptions::HyacinthError }
+      let(:error_message) { 'This is the error' }
+      before do
+        allow_any_instance_of(query_class).to receive(:resolve).and_raise(error_class, error_message)
+        # skipping authorization because it's not part of what we're testing
+        allow(ability).to receive(:authorize!)
+      end
       it 'is rescued and the error is placed in the top level graphql response "errors" field with message and path' do
-        expect(graphql_response_as_hash.dig('errors', 0, 'message')).to eq('This is the error')
+        expect(graphql_response_as_hash.dig('errors', 0, 'message')).to eq(error_message)
         expect(graphql_response_as_hash.dig('errors', 0, 'path')).to eq(['updateDescriptiveMetadata'])
       end
     end
--- spec/graphql/mutations/digital_object/resource/delete_resource_spec.rb
+++ spec/graphql/mutations/digital_object/resource/delete_resource_spec.rb
@@ -2,19 +2,15 @@
 
 require 'rails_helper'
 
-RSpec.describe Mutations::DigitalObject::Resource::DeleteResource, solr: true do
-  subject(:graphql_request) { HyacinthSchema.execute(query, context: context, variables: variables) }
+RSpec.describe Mutations::DigitalObject::Resource::DeleteResource do
+  let(:graphql_request) { HyacinthSchema.execute(query, context: context, variables: variables) }
   let(:context) { { current_user: FactoryBot.create(:user), ability: ability } }
 
   context 'when digital object is an Asset' do
+    include_context 'with stubbed search adapters'
     let(:digital_object) { FactoryBot.create(:asset, :with_main_resource, :with_access_resource) }
     let(:variables) { { input: { id: digital_object.uid, resourceName: 'access' } } }
-    let(:ability) do
-      abil = double
-      # skipping authorization because it's not part of what we're testing
-      allow(abil).to receive(:authorize!)
-      abil
-    end
+    let(:ability) { instance_double(Ability) }
     let(:query) do
       <<~GQL
         mutation ($input: DeleteResourceInput!) {
@@ -29,15 +25,12 @@ RSpec.describe Mutations::DigitalObject::Resource::DeleteResource, solr: true do
 
     before do
       allow(::DigitalObject).to receive(:find_by_uid!).and_return(digital_object)
-      allow(ability.authorize!)
+      # skipping authorization because it's not part of what we're testing
+      allow(ability).to receive(:authorize!)
     end
 
-    # TODO: Need to update to the way that digital objects save, since deep_clone method
-    # (which uses Marshal.dump) isn't compatible with rspec mocks.
     it 'sets the flag to skip resource request callbacks (to avoid regenerating the just-deleted resource)' do
-      # TODO: Use commented out code instead when DigitalObject#save method works with rspec mocks
-      # expect(digital_object).to receive(:skip_resource_request_callbacks=).with(true)
-      # TODO: Replace line below with line above
+      expect(digital_object).to receive(:skip_resource_request_callbacks=).with(true).and_call_original
       expect(ResourceRequests::AccessJob).not_to receive(:perform_later_if_eligible)
 
       graphql_request
