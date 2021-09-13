79492e41426a1b7414035e7077160365dfd08e21
Benjamin Armintor <armintor@gmail.com>
2021-09-13 14:23:47 -0400

    fix linter issues in metadata/FieldGroup.js

--- app/javascript/src/hyacinth_v1/components/digital_objects/metadata/FieldGroup.js
+++ app/javascript/src/hyacinth_v1/components/digital_objects/metadata/FieldGroup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 import produce from 'immer';
 
@@ -9,7 +10,6 @@ import RemoveButton from '../../shared/buttons/RemoveButton';
 import UpArrowButton from '../../shared/buttons/UpArrowButton';
 import DownArrowButton from '../../shared/buttons/DownArrowButton';
 
-
 const FieldGroup = (props) => {
   const {
     index,
@@ -19,7 +19,9 @@ const FieldGroup = (props) => {
     value,
     onChange,
     defaultValue,
-    dynamicFieldGroup: { stringKey, displayLabel, children, isRepeatable }
+    dynamicFieldGroup: {
+      stringKey, displayLabel, children, isRepeatable,
+    },
   } = props;
 
   const onChangeWrapper = (fieldName, fieldVal) => {
@@ -37,10 +39,18 @@ const FieldGroup = (props) => {
         {
           isRepeatable && (
             <>
-              <span className="float-end"><AddButton tabIndex="-1" className="field-group-header-button" onClick={addHandler} /></span>
-              <span className="float-end"><UpArrowButton tabIndex="-1" className="field-group-header-button" onClick={() => moveHandler('up')} /></span>
-              <span className="float-end"><DownArrowButton tabIndex="-1" className="field-group-header-button" onClick={() => moveHandler('down')} /></span>
-              <span className="float-end"><RemoveButton tabIndex="-1" className="field-group-header-button" onClick={removeHandler} /></span>
+              <span className="float-end">
+                <AddButton tabIndex="-1" className="field-group-header-button" onClick={addHandler} />
+              </span>
+              <span className="float-end">
+                <UpArrowButton tabIndex="-1" className="field-group-header-button" onClick={() => moveHandler('up')} />
+              </span>
+              <span className="float-end">
+                <DownArrowButton tabIndex="-1" className="field-group-header-button" onClick={() => moveHandler('down')} />
+              </span>
+              <span className="float-end">
+                <RemoveButton tabIndex="-1" className="field-group-header-button" onClick={removeHandler} />
+              </span>
             </>
           )
         }
@@ -54,10 +64,11 @@ const FieldGroup = (props) => {
                 return (
                   <FieldGroupArray
                     key={c.stringKey}
+                    component={FieldGroup}
                     value={value[c.stringKey]}
                     defaultValue={defaultValue[c.stringKey][0]}
                     dynamicFieldGroup={c}
-                    onChange={v => onChangeWrapper(c.stringKey, v)}
+                    onChange={(v) => onChangeWrapper(c.stringKey, v)}
                   />
                 );
               case 'DynamicField':
@@ -66,7 +77,7 @@ const FieldGroup = (props) => {
                     key={c.stringKey}
                     value={value[c.stringKey]}
                     dynamicField={c}
-                    onChange={v => onChangeWrapper(c.stringKey, v)}
+                    onChange={(v) => onChangeWrapper(c.stringKey, v)}
                   />
                 );
               default:
@@ -77,6 +88,30 @@ const FieldGroup = (props) => {
       </Card.Body>
     </Card>
   );
+};
+
+FieldGroup.propTypes = {
+  index: PropTypes.number.isRequired,
+  addHandler: PropTypes.func,
+  removeHandler: PropTypes.func,
+  moveHandler: PropTypes.func,
+  value: PropTypes.objectOf(PropTypes.any).isRequired,
+  onChange: PropTypes.func.isRequired,
+  defaultValue: PropTypes.objectOf(PropTypes.any).isRequired,
+  dynamicFieldGroup: PropTypes.shape(
+    {
+      stringKey: PropTypes.string.isRequired,
+      displayLabel: PropTypes.string.isRequired,
+      children: PropTypes.arrayOf(PropTypes.any).isRequired,
+      isRepeatable: PropTypes.bool,
+    },
+  ).isRequired,
+};
+
+FieldGroup.defaultProps = {
+  addHandler: () => {},
+  moveHandler: () => {},
+  removeHandler: () => {},
 };
 
 export default FieldGroup;
