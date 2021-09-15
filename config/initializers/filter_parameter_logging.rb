24c6658f8c96b87d004eb691c02189e417f9461c
Eric O <eric@ejdigitalmedia.com>
2021-09-15 19:37:12 -0400

    HYACINTH-882 - Update Rails filter_parameters so that useful values aren't suppressed in our logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,15 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+  :passw,
+  :secret,
+  :token,
+  # it's good to filter out things that match '_key', but we don't want to filter out things that
+  # contain 'string_key' or 'keyword'
+  /(?<!string)_key(?!word)/,
+  :crypt,
+  :salt,
+  :certificate,
+  :otp,
+  :ssn
 ]
