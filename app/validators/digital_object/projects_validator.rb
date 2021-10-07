880cdf1dc11d536dfb8507356755ae48489b0f00
Benjamin Armintor <armintor@gmail.com>
2021-10-07 16:41:52 -0400

    simplify DigitalObject::ProjectsValidator - remove unnecessary return values - exit early if no errors

--- app/validators/digital_object/projects_validator.rb
+++ app/validators/digital_object/projects_validator.rb
@@ -3,10 +3,7 @@
 class DigitalObject::ProjectsValidator < ActiveModel::Validator
   def validate(digital_object)
     primary_string_key = digital_object.primary_project&.string_key
-    if digital_object.other_projects&.detect { |proj| proj.string_key == primary_string_key }
-      digital_object.errors.add(:other_projects, "Other projects cannot also be primary: #{primary_string_key}")
-      return false
-    end
-    true
+    return unless digital_object.other_projects&.detect { |proj| proj.string_key == primary_string_key }
+    digital_object.errors.add(:other_projects, "Other projects cannot also be primary: #{primary_string_key}")
   end
 end
