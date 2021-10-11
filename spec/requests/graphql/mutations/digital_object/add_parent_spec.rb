580760c9a94d63e0eee6a513eea2ff810d4c738a
Eric O <eric@ejdigitalmedia.com>
2021-10-11 17:15:04 -0400

    HYACINTH-911 - Convert add_parent_spec and remove_parent_spec tests to request tests and also test permissions

--- spec/requests/graphql/mutations/digital_object/add_parent_spec.rb
+++ spec/requests/graphql/mutations/digital_object/add_parent_spec.rb
@@ -2,13 +2,37 @@
 
 require 'rails_helper'
 
-RSpec.describe Mutations::DigitalObject::AddParent, solr: true do
-  subject(:graphql_request) { HyacinthSchema.execute(query, context: context, variables: variables) }
-  let(:context) { { current_user: FactoryBot.create(:user), ability: ability } }
-  let(:ability) { instance_double(Ability) }
-  let(:parent) { FactoryBot.create(:item) }
-  let(:item) { FactoryBot.create(:item) }
-  let(:query) do
+RSpec.describe Mutations::DigitalObject::AddParent, type: :request do
+  include_context 'with stubbed search adapters'
+
+  let(:parent_project) { FactoryBot.create(:project) }
+  let(:child_project) { FactoryBot.create(:project) }
+  let(:parent) { FactoryBot.create(:item, primary_project: parent_project) }
+  let(:child) { FactoryBot.create(:asset, :with_main_resource, primary_project: child_project) }
+  let(:variables) { { input: { id: child.uid, parentId: parent.uid } } }
+
+  include_examples 'a basic user with no abilities is not authorized to perform this request', 'You do not have permission to add the specified parent-child relationship' do
+    let(:request) { graphql query, variables }
+  end
+
+  context "when logged in user has appropriate permissions" do
+    before do
+      sign_in_project_contributor actions: [:read_objects, :update_objects], projects: [parent_project, child_project]
+      graphql query, variables
+      child.reload
+    end
+
+    it "returns the expected response" do
+      expect(response.body).to be_json_eql("\"#{child.uid}\"").at_path('data/addParent/digitalObject/id')
+    end
+
+    it "adds the correct parent" do
+      expect(child.parents.length).to eq(1)
+      expect(child.parents[0].uid).to eq(parent.uid)
+    end
+  end
+
+  def query
     <<~GQL
       mutation ($input: AddParentInput!) {
         addParent(input: $input) {
@@ -18,19 +42,5 @@ RSpec.describe Mutations::DigitalObject::AddParent, solr: true do
         }
       }
     GQL
-  end
-  before do
-    # skipping authorization because it's not part of what we're testing
-    allow(ability).to receive(:authorize!)
-  end
-
-  context 'when user has correct permissions' do
-    let(:variables) { { input: { id: item.uid, parentId: parent.uid } } }
-    it 'adds the correct parent' do
-      graphql_request
-      item.reload
-      expect(item.parents.length).to eq(1)
-      expect(item.parents[0].uid).to eq(parent.uid)
-    end
   end
 end
--- spec/requests/graphql/mutations/digital_object/remove_parent_spec.rb
+++ spec/requests/graphql/mutations/digital_object/remove_parent_spec.rb
@@ -2,13 +2,37 @@
 
 require 'rails_helper'
 
-RSpec.describe Mutations::DigitalObject::RemoveParent, solr: true do
-  subject(:graphql_request) { HyacinthSchema.execute(query, context: context, variables: variables) }
-  let(:context) { { current_user: FactoryBot.create(:user), ability: ability } }
-  let(:ability) { instance_double(Ability) }
-  let(:parent) { FactoryBot.create(:item) }
-  let(:item) { FactoryBot.create(:item) }
-  let(:query) do
+RSpec.describe Mutations::DigitalObject::RemoveParent, type: :request do
+  include_context 'with stubbed search adapters'
+
+  let(:parent_project) { FactoryBot.create(:project) }
+  let(:child_project) { FactoryBot.create(:project) }
+  let(:parent) { FactoryBot.create(:item, primary_project: parent_project) }
+  let(:child) { FactoryBot.create(:asset, :with_main_resource, primary_project: child_project, parents_to_add: [parent]) }
+  let(:variables) { { input: { id: child.uid, parentId: parent.uid } } }
+
+  include_examples 'a basic user with no abilities is not authorized to perform this request', 'You do not have permission to remove this parent-child relationship' do
+    let(:request) { graphql query, variables }
+  end
+
+  context "when logged in user has appropriate permissions" do
+    before do
+      sign_in_project_contributor actions: [:update_objects], projects: [parent_project]
+      graphql query, variables
+      child.reload
+    end
+
+    it "returns the expected response" do
+      expect(response.body).to be_json_eql("\"#{child.uid}\"").at_path('data/removeParent/digitalObject/id')
+    end
+
+    it "removes the correct parent" do
+      child.reload
+      expect(child.parents.length).to eq(0)
+    end
+  end
+
+  def query
     <<~GQL
       mutation ($input: RemoveParentInput!) {
         removeParent(input: $input) {
@@ -18,20 +42,5 @@ RSpec.describe Mutations::DigitalObject::RemoveParent, solr: true do
         }
       }
     GQL
-  end
-  before do
-    # skipping authorization because it's not part of what we're testing
-    allow(ability).to receive(:authorize!)
-    item.parents_to_add << parent
-    item.save
-  end
-
-  context 'when user has correct permissions' do
-    let(:variables) { { input: { id: item.uid, parentId: parent.uid } } }
-    it 'removes the correct parent' do
-      graphql_request
-      item.reload
-      expect(item.parents.length).to eq(0)
-    end
   end
 end
