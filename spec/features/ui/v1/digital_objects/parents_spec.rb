3e0f13c3b0ce29640835c261fe5dbd57821740e1
JackBlackLight <jd2148@columbia.edu>
2021-10-18 17:44:09 -0400

    feature spec for add parent permissions

--- spec/features/ui/v1/digital_objects/parents_spec.rb
+++ spec/features/ui/v1/digital_objects/parents_spec.rb
@@ -25,5 +25,40 @@ RSpec.describe 'Digital Object Parents', solr: true, type: :feature, js: true do
         end
       end
     end
+    context 'when logged in user does not have update permission on a parent' do
+      context 'trying to add the parent' do
+        let(:permissions_required) { [:read_objects] }
+        let(:new_item) { FactoryBot.create(:item, :with_ascii_title) }
+        let(:request_url) { "/ui/v1/digital_objects/#{asset.uid}/parents" }
+        before do
+          visit request_url
+          page.find_field('addParentInput').set ""
+          fill_in("addParentInput", with: new_item.uid)
+          find_button('addParentButton').click
+        end
+        it "returns an error" do
+          expect(page).to have_content("You do not have permission to add the specified parent-child relationship")
+        end
+      end
+    end
+    context 'when logged in user has update permission on one parent but not another' do
+      context 'remove buttons' do
+        let(:permissions_required) { [:read_objects] }
+        let(:request_url) { "/ui/v1/digital_objects/#{asset.uid}/parents" }
+        before do
+          visit request_url
+        end
+        it "are displayed for the permitted parent" do
+          parents_header = page.find("h4", text: "Parent Digital Objects")
+          expect(parents_header).not_to be_nil
+          expect(parents_header).to have_sibling(".card", text: item.generate_display_label)
+        end
+        it "and not displayed for the not permitted parent" do
+          parents_header = page.find("h4", text: "Parent Digital Objects")
+          expect(parents_header).not_to be_nil
+          expect(parents_header).to have_sibling(".card", text: item.generate_display_label)
+        end
+      end
+    end
   end
 end
