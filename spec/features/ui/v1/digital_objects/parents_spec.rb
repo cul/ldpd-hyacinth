568f114a3cad7a539600aa33d2bcc021df586f23
JackBlackLight <jd2148@columbia.edu>
2021-10-29 12:15:24 -0400

    add specs for successful parent add and remove

--- spec/features/ui/v1/digital_objects/parents_spec.rb
+++ spec/features/ui/v1/digital_objects/parents_spec.rb
@@ -5,16 +5,17 @@ require 'rails_helper'
 RSpec.describe 'Digital Object Parents', type: :feature, js: true do
   include_context 'with stubbed search adapters'
   let(:item) { FactoryBot.create(:item, :with_ascii_title) }
+  let(:another_item) { FactoryBot.create(:item, :with_ascii_title) }
   let(:asset) { FactoryBot.create(:asset, :with_main_resource, :with_ascii_title, parents_to_add: [item]) }
-
+  let(:projects) { item.primary_project }
   let(:permissons_required) { [] }
+  let(:request_url) { "/ui/v1/digital_objects/#{asset.uid}/parents" }
 
-  before { sign_in_project_contributor actions: permissions_required, projects: item.primary_project }
+  before { sign_in_project_contributor actions: permissions_required, projects: projects }
 
   describe 'GET /ui/v1/digital_objects/:uid/parents' do
     context 'when logged in user has appropriate permissions' do
       let(:permissions_required) { [:read_objects] }
-      let(:request_url) { "/ui/v1/digital_objects/#{asset.uid}/parents" }
       before do
         visit request_url
       end
@@ -24,34 +25,53 @@ RSpec.describe 'Digital Object Parents', type: :feature, js: true do
         expect(page).to have_css('.parent .uid-header', text: item.generate_display_label)
       end
     end
-    context 'when a logged in user with object read permission tries to add a parent and they do not have update permission for that parent' do
-      let(:new_item) { FactoryBot.create(:item, :with_ascii_title) }
-      let(:permissions_required) { [:read_objects] }
-      let(:request_url) { "/ui/v1/digital_objects/#{asset.uid}/parents" }
+    context 'when a logged in user with update permission for an item tries to add it as a parent' do
+      let(:permissions_required) { [:read_objects, :update_objects] }
+      let(:projects) { [item.primary_project, another_item.primary_project] }
       before do
         visit request_url
         page.find_field('addParentInput').set ""
-        fill_in("addParentInput", with: new_item.uid)
+        fill_in("addParentInput", with: another_item.uid)
+        find_button('addParentButton').click
+      end
+      it "page displays added parent label" do
+        expect(page).to have_css('.parent .uid-header', text: another_item.generate_display_label)
+      end
+    end
+    context 'when an authorized user removes a parent' do
+      let(:permissions_required) { [:read_objects, :update_objects] }
+      before do
+        visit request_url
+        find_button("remove_parent_#{item.uid}").click
+      end
+      it "page does not display parent label" do
+        expect(page).not_to have_css('.parent .uid-header', text: item.generate_display_label)
+      end
+    end
+    context 'when a logged in user tries to add a parent and they do not have update permission for that parent' do
+      let(:permissions_required) { [:read_objects] }
+      before do
+        visit request_url
+        page.find_field('addParentInput').set ""
+        fill_in("addParentInput", with: another_item.uid)
         find_button('addParentButton').click
       end
       it "returns an error" do
         expect(page).to have_content("You do not have permission to add the specified parent-child relationship")
       end
     end
-    context 'when an object has two parents from different projects, and the logged in user only has update permission for one of the parent objects' do
-      let(:new_item) { FactoryBot.create(:item, :with_ascii_title) }
+    context 'when an object has parents from different projects, and the logged in user has update permission for only one of the parents' do
       let(:permissions_required) { [:read_objects, :update_objects] }
-      let(:request_url) { "/ui/v1/digital_objects/#{asset.uid}/parents" }
       before do
-        asset.parents_to_add << new_item
+        asset.parents_to_add << another_item
         asset.save
         visit request_url
       end
+      it "does not display a remove button for the unauthorized parent" do
+        expect(page).not_to have_button("remove_parent_#{another_item.uid}")
+      end
       it "displays a remove button for the authorized parent" do
         expect(page).to have_button("remove_parent_#{item.uid}")
-      end
-      it "does not display a remove button for the unauthorized parent" do
-        expect(page).not_to have_button("remove_parent_#{new_item.uid}")
       end
     end
   end
