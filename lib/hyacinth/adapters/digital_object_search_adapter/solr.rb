ea6662aa9bc58c0a08aaa7bd1f255f2272098180
Eric O <eric@ejdigitalmedia.com>
2021-11-09 19:58:53 -0500

    In DigitalObjectSearchAdapter::Solr, undo private method designation in favor of YARD documentation

--- lib/hyacinth/adapters/digital_object_search_adapter/solr.rb
+++ lib/hyacinth/adapters/digital_object_search_adapter/solr.rb
@@ -83,41 +83,48 @@ module Hyacinth
           solr.clear
         end
 
-        private
+        # Generates a Solr::Params object for the given search_params.
+        # @param [Hash] search_params
+        # @return [Solr::Params]
+        # @!visibility private
+        def solr_params_for(search_params)
+          solr_parameters = ::Solr::Params.new
+          # Only return active objects
+          solr_parameters.fq('state_ssi', 'active')
 
-          def solr_params_for(search_params)
-            solr_parameters = ::Solr::Params.new
-            # Only return active objects
-            solr_parameters.fq('state_ssi', 'active')
-
-            # Apply search_params
-            search_params.each do |k, v|
-              if k.to_s == 'q'
-                solr_parameters.q(v)
-              elsif k.to_s == 'search_type'
-                solr_parameters.default_field(document_generator.search_field(v)) if document_generator.search_field(v)
-              elsif k.to_s == 'facet_on'
-                Array(v).map { |eachv| solr_parameters.facet_on(eachv) }
-              else
-                Array(v).map { |eachv| solr_parameters.fq(k, *eachv) }
-              end
-            end
-            solr_parameters
-          end
-
-          # Adds filter queries to the given solr_prameters based on projects where the
-          # given user has read access.
-          def apply_user_project_filters(solr_parameters, user)
-            read_objects_access_project_string_keys = Project.accessible_by(Ability.new(user), :read_objects).map(&:string_key)
-            if read_objects_access_project_string_keys.present?
-              solr_parameters.fq('projects_ssim', read_objects_access_project_string_keys)
+          # Apply search_params
+          search_params.each do |k, v|
+            if k.to_s == 'q'
+              solr_parameters.q(v)
+            elsif k.to_s == 'search_type'
+              solr_parameters.default_field(document_generator.search_field(v)) if document_generator.search_field(v)
+            elsif k.to_s == 'facet_on'
+              Array(v).map { |eachv| solr_parameters.facet_on(eachv) }
             else
-              # User has no project permissions, so we'll deliberately put in an unresolvable value
-              # to get zero results. Projects cannot have exclamation points in their string keys,
-              # so the value below will work.
-              solr_parameters.fq('projects_ssim', '!nomatch!')
+              Array(v).map { |eachv| solr_parameters.fq(k, *eachv) }
             end
           end
+          solr_parameters
+        end
+
+        # Adds filter queries to the given solr_prameters based on projects where the
+        # given user has read access.
+
+        # Generates a Solr::Params object for the given search_params.
+        # @param [Solr::Params] solr_parameters
+        # @param [User] user
+        # @!visibility private
+        def apply_user_project_filters(solr_parameters, user)
+          read_objects_access_project_string_keys = Project.accessible_by(Ability.new(user), :read_objects).map(&:string_key)
+          if read_objects_access_project_string_keys.present?
+            solr_parameters.fq('projects_ssim', read_objects_access_project_string_keys)
+          else
+            # User has no project permissions, so we'll deliberately put in an unresolvable value
+            # to get zero results. Projects cannot have exclamation points in their string keys,
+            # so the value below will work.
+            solr_parameters.fq('projects_ssim', '!nomatch!')
+          end
+        end
       end
     end
   end
