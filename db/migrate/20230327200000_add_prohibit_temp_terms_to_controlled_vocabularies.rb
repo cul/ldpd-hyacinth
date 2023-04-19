c19271addf20d48bc8d7b27ed356b949de326a88
Benjamin Armintor <armintor@gmail.com>
2023-04-19 16:44:42 -0400

    Remove unnecessary index on prohibit_temp_terms column
    Co-authored-by: Eric O <elohanlon@users.noreply.github.com>

--- db/migrate/20230327200000_add_prohibit_temp_terms_to_controlled_vocabularies.rb
+++ db/migrate/20230327200000_add_prohibit_temp_terms_to_controlled_vocabularies.rb
@@ -1,7 +1,7 @@
 class AddProhibitTempTermsToControlledVocabularies < ActiveRecord::Migration
   def change
     change_table(:controlled_vocabularies) do |t|
-      t.boolean :prohibit_temp_terms, default: false, null: false, index: true
+      t.boolean :prohibit_temp_terms, default: false, null: false
     end
   end
 end
