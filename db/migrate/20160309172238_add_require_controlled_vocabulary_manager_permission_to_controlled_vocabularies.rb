9c0c10f18d1f0641404747e6035a107c9e3f6da5
Benjamin Armintor <armintor@gmail.com>
2023-12-18 22:26:40 -0500

    explicitly name a shorter db index field name on controlled vocabularies

--- db/migrate/20160309172238_add_require_controlled_vocabulary_manager_permission_to_controlled_vocabularies.rb
+++ db/migrate/20160309172238_add_require_controlled_vocabulary_manager_permission_to_controlled_vocabularies.rb
@@ -2,7 +2,7 @@ class AddRequireControlledVocabularyManagerPermissionToControlledVocabularies < 
   def change
     change_table(:controlled_vocabularies) do |t|
       t.remove :only_managed_by_admins
-      t.boolean :require_controlled_vocabulary_manager_permission, default: false, null: false, index: true
+      t.boolean :require_controlled_vocabulary_manager_permission, default: false, null: false, index: { name: "idx_controlled_vocabularies_require_manager_permission"}
     end
   end
 end
