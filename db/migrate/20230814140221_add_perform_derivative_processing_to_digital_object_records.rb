7e3537ca56a2dbd20c8e07ba1f88dca48d4bcb17
Eric O <eric@ejdigitalmedia.com>
2024-03-31 13:19:45 -0400

    Remove irrelevant copy/pasted comment

--- db/migrate/20230814140221_add_perform_derivative_processing_to_digital_object_records.rb
+++ db/migrate/20230814140221_add_perform_derivative_processing_to_digital_object_records.rb
@@ -4,8 +4,6 @@ class AddPerformDerivativeProcessingToDigitalObjectRecords < ActiveRecord::Migra
       t.boolean :perform_derivative_processing, null: false, default: false
     end
 
-    # Add index to csv_row_number so that we can find the record with
-    # csv_row_number equal to another records's prerequisite_csv_row_numbers
     add_index :digital_object_records, :perform_derivative_processing
   end
 end
