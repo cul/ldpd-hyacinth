ae11940b06db799199a129ef0d9e6f7ad447d075
Benjamin Armintor <armintor@gmail.com>
2024-08-07 17:15:39 -0400

    WIP update FCRepo3 libs to support delegating external content manager

--- docker/fedora/setup-and-start.sh
+++ docker/fedora/setup-and-start.sh
@@ -7,4 +7,16 @@ if [ ! -f /opt/fedora/tomcat/bin/catalina.sh ]; then
   echo 'Done running installer jar...'
 fi
 
+rm /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/httpclient-4*.jar
+rm /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/httpcore-4*.jar
+rm /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/httpclient-4*.jar
+cp /opt/apache-http/*.jar /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/
+cp /opt/s3-url-protocol-1.0-SNAPSHOT.jar /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/
+cp /opt/awssdk/*.jar /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/
+cp /opt/fcrepo3-s3-server-1.0-SNAPSHOT.jar /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/
+cp /opt/s3-2.26.27.jar /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/
+cp /opt/aws-core-2.26.27.jar /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/
+cp /opt/fedora.delegating-external.fcfg /opt/fedora/server/config/fedora.fcfg
+
 /opt/fedora/tomcat/bin/catalina.sh run
+
