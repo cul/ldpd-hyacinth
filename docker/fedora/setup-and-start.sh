b35b57e43f86eda066a4cf229ed2c193f1e68b4e
Benjamin Armintor <armintor@gmail.com>
2024-08-08 00:07:07 -0400

    maunally unpack fedora WAR to set up library overrides before starting Tomcat

--- docker/fedora/setup-and-start.sh
+++ docker/fedora/setup-and-start.sh
@@ -7,7 +7,13 @@ if [ ! -f /opt/fedora/tomcat/bin/catalina.sh ]; then
   echo 'Done running installer jar...'
 fi
 
-/opt/fedora/tomcat/bin/catalina.sh run
+echo "Manually unpacking WAR to override libraries"
+mkdir -p /opt/fedora/webapp-tmp/fedora
+mv /opt/fedora/tomcat/webapps/fedora.war /opt/fedora/webapp-tmp/fedora/
+cd /opt/fedora/webapp-tmp/fedora
+jar -xvf fedora.war
+cd /opt
+mv /opt/fedora/webapp-tmp/fedora /opt/fedora/tomcat/webapps/
 
 rm /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/httpclient-4*.jar
 rm /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/httpcore-4*.jar
@@ -16,5 +22,7 @@ cp /opt/jars/apache-http/*.jar /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/
 cp /opt/jars/cul/s3-url-protocol-1.0-SNAPSHOT.jar /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/
 cp /opt/jars/awssdk/*.jar /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/
 cp /opt/jars/cul/fcrepo3-s3-server-1.0-SNAPSHOT.jar /opt/fedora/tomcat/webapps/fedora/WEB-INF/lib/
+echo "Done overriding Fedora 3 libraries; setting new FCFG config"
 cp /opt/fedora.delegating-external.fcfg /opt/fedora/server/config/fedora.fcfg
-touch /opt/fedora/tomcat/webapps/fedora/WEB-INF/web.xml
+
+/opt/fedora/tomcat/bin/catalina.sh run
