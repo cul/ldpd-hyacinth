d6abd0b96f992a894115a436719d87cb894c2991
Eric O <eric@ejdigitalmedia.com>
2024-12-23 16:07:44 -0500

    Symlink gcp.yml during deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,7 @@ set :deploy_to, "/opt/passenger/#{fetch(:deploy_name)}"
 # Default value for :linked_files is []
 append  :linked_files,
         'config/aws.yml',
+        'config/gcp.yml',
         'config/master.key',
         'config/database.yml',
         'config/fedora.yml',
