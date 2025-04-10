a9cfb4988e24c3fab3bca66ea08cf8ccff26cc48
Frederic Duby <fcd1@columbia.edu>
2025-04-10 15:15:48 -0400

    WIP: update spec to use new fixture

--- spec/unit/hyacinth/datacite/datacite_metadata_builder_spec.rb
+++ spec/unit/hyacinth/datacite/datacite_metadata_builder_spec.rb
@@ -7,12 +7,8 @@ describe Hyacinth::Datacite::DataciteMetadataBuilder do
 
   let(:metadata_retrieval) { Hyacinth::Datacite::HyacinthMetadata.new dod }
 
-  let(:schema) do
-     Nokogiri::XML::Schema(fixture('xsd/datacite/kernel-3.xsd'))
-  end
-
   context "datacite_json" do
-    let(:dod) { JSON.parse( fixture('lib/hyacinth/ezid/ezid_item.json').read ) }
+    let(:dod) { JSON.parse( fixture('lib/hyacinth/datacite/hyacinth_item.json').read ) }
     let(:expected_attributes_hash) do
       { titles: [{ title: "The Catcher in the Rye" }],
         publisher: "Columbia University",
