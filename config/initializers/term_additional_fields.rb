94203019272e78f6a51cb2adc8555244bc86e777
Eric O <eric@ejdigitalmedia.com>
2025-06-06 14:09:17 -0400

    TERM_ADDITIONAL_FIELDS config needs to be a hash with indifferent access

--- config/initializers/term_additional_fields.rb
+++ config/initializers/term_additional_fields.rb
@@ -1,1 +1,1 @@
-TERM_ADDITIONAL_FIELDS = Rails.application.config_for('term_additional_fields')
+TERM_ADDITIONAL_FIELDS = Rails.application.config_for('term_additional_fields').with_indifferent_access
