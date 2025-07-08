5c1f01421e6c296534a4646e58e9cf03a5868c2a
Eric O <eric@ejdigitalmedia.com>
2025-07-08 18:30:39 -0400

    Fix for TermsController create and update actions

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -35,7 +35,7 @@ class TermsController < ApplicationController
     unless @errors.present?
 
       type = term_prms.delete('type')
-      term_opts = flat_term_data_to_term_opts(term_prms)
+      term_opts = flat_term_data_to_term_opts(term_prms.to_unsafe_h)
 
       begin
         @term = UriService.client.create_term(type, term_opts)
@@ -63,7 +63,7 @@ class TermsController < ApplicationController
   # PATCH/PUT /terms/1.json
   def update
     @errors = []
-    term_opts = flat_term_data_to_term_opts(term_params)
+    term_opts = flat_term_data_to_term_opts(term_params.to_unsafe_h)
 
     begin
       @term = UriService.client.update_term(@term['uri'], term_opts)
