6b7eea5d7af451a1c630aa550e8c585bc59f70f1
Eric O <eric@ejdigitalmedia.com>
2025-07-23 15:39:12 -0400

    Update git branch suggestion logic in test.rb and prod.rb deployment files

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,3 +1,3 @@
 server 'hy-rails-prod1.cul.columbia.edu', user: fetch(:remote_user), roles: %w[app db web]
 # In test/prod, deploy from release tags; most recent version is default
-ask :branch, proc { `git tag --sort=version:refname`.split("\n").last }
+ask :branch, proc { `git tag --sort=-creatordate | head -n 1`.split("\n").last }
